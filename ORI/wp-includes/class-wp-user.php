8076898429c64c1036a9179f26483c23d5f4a234
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-09 21:12:36 +0900

    Add class: WP_User

--- ORI/wp-includes/class-wp-user.php
+++ ORI/wp-includes/class-wp-user.php
@@ -36,76 +36,15 @@
  * @property string $syntax_highlighting
  */
 class WP_User {
-	/**
-	 * User data container.
-	 *
-	 * @since 2.0.0
-	 * @var object
-	 */
-	public $data;
-
-	/**
-	 * The user's ID.
-	 *
-	 * @since 2.1.0
-	 * @var int
-	 */
-	public $ID = 0;
-
-	/**
-	 * The individual capabilities the user has been given.
-	 *
-	 * @since 2.0.0
-	 * @var array
-	 */
-	public $caps = array();
-
-	/**
-	 * User metadata option name.
-	 *
-	 * @since 2.0.0
-	 * @var string
-	 */
-	public $cap_key;
-
-	/**
-	 * The roles the user is part of.
-	 *
-	 * @since 2.0.0
-	 * @var array
-	 */
-	public $roles = array();
-
-	/**
-	 * All capabilities the user has, including individual and role based.
-	 *
-	 * @since 2.0.0
-	 * @var array
-	 */
-	public $allcaps = array();
-
-	/**
-	 * The filter context applied to user data fields.
-	 *
-	 * @since 2.9.0
-	 * @var string
-	 */
-	public $filter = null;
-
-	/**
-	 * The site ID the capabilities of this user are initialized for.
-	 *
-	 * @since 4.9.0
-	 * @var int
-	 */
-	private $site_id = 0;
-
-	/**
-	 * @static
-	 * @since 3.3.0
-	 * @var array
-	 */
-	private static $back_compat_keys;
+	// refactored. public $data;
+	// refactored. public $ID = 0;
+	// refactored. public $caps = array();
+	// refactored. public $cap_key;
+	// refactored. public $roles = array();
+	// refactored. public $allcaps = array();
+	// refactored. public $filter = null;
+	// refactored. private $site_id = 0;
+	// refactored. private static $back_compat_keys;
 
 	/**
 	 * Constructor.
--- REF/wp-includes/class-wp-user.php
+++ REF/wp-includes/class-wp-user.php
@@ -1,2 +1,98 @@
 <?php
-// @NOW 015
+/**
+ * User API: WP_User class
+ *
+ * @package    WordPress
+ * @subpackage Users
+ * @since      4.4.0
+ */
+
+/**
+ * Core class used to implement the WP_User object.
+ *
+ * @since 2.0.0
+ */
+class WP_User
+{
+	/**
+	 * User data container.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @var object
+	 */
+	public $data;
+
+	/**
+	 * The user's ID.
+	 *
+	 * @since 2.1.0
+	 *
+	 * @var int
+	 */
+	public $ID = 0;
+
+	/**
+	 * The individual capabilities the user has been given.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @var array
+	 */
+	public $caps = [];
+
+	/**
+	 * User metadata option name.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @var string
+	 */
+	public $cap_key;
+
+	/**
+	 * The roles the user is part of.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @var array
+	 */
+	public $roles = [];
+
+	/**
+	 * All capabilities the user has, including individual and role based.
+	 *
+	 * @since 2.0.0
+	 *
+	 * @var array
+	 */
+	public $allcaps = [];
+
+	/**
+	 * The filter context applied to user data fields.
+	 *
+	 * @since 2.9.0
+	 *
+	 * @var string
+	 */
+	public $filter = NULL;
+
+	/**
+	 * The site ID the capabilities of this user are initialized for.
+	 *
+	 * @since 4.9.0
+	 *
+	 * @var int
+	 */
+	private $site_id = 0;
+
+	/**
+	 * @static
+	 * @since  3.3.0
+	 *
+	 * @var array
+	 */
+	private static $back_compat_keys;
+
+	// @NOW 015
+}
