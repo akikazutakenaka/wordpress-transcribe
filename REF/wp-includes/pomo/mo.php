e69852b6bda1312f463edd1df780c22c7a480084
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-09 16:15:24 +0900

    Add file: wp-includes/pomo/translations.php

--- REF/wp-includes/pomo/mo.php
+++ REF/wp-includes/pomo/mo.php
@@ -1,2 +1,11 @@
 <?php
-// @NOW 006
+/**
+ * Class for working with MO files
+ *
+ * @version    $Id: mo.php 1157 2015-11-20 04:30:11Z dd32 $
+ * @package    pomo
+ * @subpackage mo
+ */
+
+require_once dirname( __FILE__ ) . '/translations.php';
+// @NOW 006 -> wp-includes/pomo/translations.php
