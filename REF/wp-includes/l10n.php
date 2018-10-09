8998e8873ac8a01baaada13ba7764d854827957d
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-09 20:04:27 +0900

    Add function: _load_textdomain_just_in_time( string $domain )

--- REF/wp-includes/l10n.php
+++ REF/wp-includes/l10n.php
@@ -127,7 +127,34 @@ function load_textdomain( $domain, $mofile )
 	return TRUE;
 }
 
-// @NOW 010
+/**
+ * Loads plugin and theme textdomains just-in-time.
+ *
+ * When a textdomain is encountered for the first time, we try to load the translation file from `wp-content/languages`, removing the need to call load_plugin_textdomain() or load_theme_textdomain().
+ *
+ * @since  4.6.0
+ * @access private
+ * @see    get_translations_for_domain()
+ * @global array $l10n_unloaded An array of all text domains that have been unloaded again.
+ *
+ * @param  string $domain Text domain.
+ *                        Unique identifier for retrieving translated strings.
+ * @return bool   True when the textdomain is successfully loaded, false otherwise.
+ */
+function _load_textdomain_just_in_time( $domain )
+{
+	global $l10n_unloaded;
+	$l10n_unloaded = ( array ) $l10n_unloaded;
+
+	// Short-circuit if domain is 'default' which is reserved for core.
+	if ( 'default' === $domain || isset( $l10n_unloaded[$domain] ) )
+		return FALSE;
+
+	$translation_path = _get_path_to_translation( $domain );
+	// @NOW 010 -> wp-includes/l10n.php
+}
+
+// @NOW 011
 
 /**
  * Return the Translations instance for a text domain.
