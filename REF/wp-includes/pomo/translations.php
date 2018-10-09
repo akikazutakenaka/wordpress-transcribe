01516e6bba95a4ef4666c0fe18edf851b1a4d530
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-09 16:17:20 +0900

    Add file: wp-includes/pomo/plural-forms.php

--- REF/wp-includes/pomo/translations.php
+++ REF/wp-includes/pomo/translations.php
@@ -1,2 +1,11 @@
 <?php
-// @NOW 007
+/**
+ * Class for a set of entries for translation and their associated headers
+ *
+ * @version    $Id: translations.php 1157 2015-11-20 04:30:11Z dd32 $
+ * @package    pomo
+ * @subpackage translations
+ */
+
+require_once dirname( __FILE__ ) . '/plural-forms.php';
+// @NOW 007 -> wp-includes/pomo/plural-forms.php
