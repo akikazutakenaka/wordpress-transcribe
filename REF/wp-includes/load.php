cf8cd414fe1e3efb6fdc8c332689508e3a0ee19b
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-09 20:34:53 +0900

    Add function: is_admin() Add file: wp-admin/includes/class-wp-screen.php

--- REF/wp-includes/load.php
+++ REF/wp-includes/load.php
@@ -26,7 +26,25 @@ function wp_check_php_mysql_version()
 	}
 }
 
-// @NOW 013
+/**
+ * Whether the current request is for an administrative interface page,
+ *
+ * Does not check if the user is an administrator; current_user_can() for checking roles and capabilities.
+ *
+ * @since  1.5.1
+ * @global WP_Screen $current_screen
+ *
+ * @return bool True if inside WordPress administration interface, false otherwise.
+ */
+function is_admin()
+{
+	return isset( $GLOBALS['current_screen'] )
+		? $GLOBALS['current_screen']->in_admin()
+		: ( defined( 'WP_ADMIN' )
+			? WP_ADMIN
+			: FALSE );
+	// @NOW 013 -> wp-admin/includes/class-wp-screen.php
+}
 
 /**
  * If Multisite is enabled.
