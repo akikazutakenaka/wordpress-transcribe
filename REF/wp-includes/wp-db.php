246d9dcf2163c16163c50cc2a52fc8262cc0f595
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-10 23:15:32 +0900

    Finish function: wpdb::set_sql_mode( [array $modes = []] )

--- REF/wp-includes/wp-db.php
+++ REF/wp-includes/wp-db.php
@@ -756,6 +756,65 @@ class wpdb
 	}
 
 	/**
+	 * Change the current SQL mode, and ensure its WordPress compatibility.
+	 *
+	 * If no modes are passed, it will ensure the current MySQL server modes are compatible.
+	 *
+	 * @since 3.9.0
+	 *
+	 * @param array $modes Optional.
+	 *                     A list of SQL modes to set.
+	 */
+	public function set_sql_mode( $modes = [] )
+	{
+		if ( empty( $modes ) ) {
+			$res = $this->use_mysqli
+				? mysqli_query( $this->dbh, 'SELECT @@SESSION.sql_mode' )
+				: mysql_query( 'SELECT @@SESSION.sql_mode', $this->dbh );
+
+			if ( empty( $res ) )
+				return;
+
+			if ( $this->use_mysqli ) {
+				$modes_array = mysqli_fetch_array( $res );
+
+				if ( empty( $modes_array[0] ) )
+					return;
+
+				$modes_str = $modes_array[0];
+			} else
+				$modes_str = mysql_result( $res, 0 );
+
+			if ( empty( $modes_str ) )
+				return;
+
+			$modes = explode( ',', $modes_str );
+		}
+
+		$modes = array_change_key_case( $modes, CASE_UPPER );
+
+		/**
+		 * Filters the list of incompatible SQL modes to exclude.
+		 *
+		 * @since 3.9.0
+		 *
+		 * @param array $incompatible_modes An array of incompatible modes.
+		 */
+		$incompatible_modes = ( array ) apply_filters( 'incompatible_sql_modes', $this->incompatible_modes );
+
+		foreach ( $modes as $i => $mode )
+			if ( in_array( $mode, $incompatible_modes ) )
+				unset( $modes[$i] );
+
+		$modes_str = implode( ',', $modes );
+
+		if ( $this->use_mysqli )
+			mysqli_query( $this->dbh, "SET SESSION sql_mode='$modes_str'" );
+		else
+			mysql_query( "SET SESSION sql_mode='$modes_str'", $this->dbh );
+	}
+
+	/**
 	 * Real escape, using mysqli_real_escape_string() or mysql_real_escape_string()
 	 *
 	 * @see   mysqli_real_escape_string()
@@ -1028,6 +1087,8 @@ class wpdb
 
 			$this->has_connected = TRUE;
 			$this->set_charset( $this->dbh );
+			$this->ready = TRUE;
+			$this->set_sql_mode();
 			// @NOW 019
 		}
 	}
