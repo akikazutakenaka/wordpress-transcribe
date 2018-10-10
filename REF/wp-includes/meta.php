62022e2904c8e9c7193c866a5ebb9637c3018f3e
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-10 23:43:07 +0900

    Add function: get_metadata( string $meta_type, int $object_id [, string $meta_key = '' [, bool $single = FALSE]] ) Add file: wp-includes/cache.php

--- REF/wp-includes/meta.php
+++ REF/wp-includes/meta.php
@@ -1,2 +1,58 @@
 <?php
-// @NOW 019
+/**
+ * Core Metadata API
+ *
+ * Functions for retrieving and manipulating metadata of various WordPress object types.
+ * Metadata for an object is a represented by a simple key-value pair.
+ * Objects may contain multiple metadata entries that share the same key and differ only in their value.
+ *
+ * @package    WordPress
+ * @subpackage Meta
+ */
+
+/**
+ * Retrieve metadata for the specified object.
+ *
+ * @since 2.9.0
+ *
+ * @param  string $meta_type Type of object metadata is for (e.g., comment, post, or user).
+ * @param  int    $object_id ID of the object metadata is for.
+ * @param  string $meta_key  Optional.
+ *                           Metadata key.
+ *                           If not specified, retrieve all metadata for the specified object.
+ * @param  bool   $single    Optional, default is false.
+ *                           If true, return only the first value of the specified meta_key.
+ *                           This parameter has no effect if meta_key is not specified.
+ * @return mixed  Single metadata value, or array of values.
+ */
+function get_metadata( $meta_type, $object_id, $meta_key = '', $single = FALSE )
+{
+	if ( ! $meta_type || ! is_numeric( $object_id ) )
+		return FALSE;
+
+	$object_id = absint( $object_id );
+
+	if ( ! $object_id )
+		return FALSE;
+
+	/**
+	 * Filters whether to retrieve metadata of a specific type.
+	 *
+	 * The dynamic portion of the hook, `$meta_type`, refers to the meta object type (comment, post, or user).
+	 * Returning a non-null value will effectively short-circuit the function.
+	 *
+	 * @since 3.1.0
+	 *
+	 * @param null|array|string $value     The value get_metadata() should return - a single metadata value, or an array of values.
+	 * @param int               $object_id Object ID.
+	 * @param string            $meta_key  Meta key.
+	 * @param bool              $single    Whether to return only the first value of the specified $meta_key.
+	 */
+	$check = apply_filters( "get_{$meta_type}_metadata", NULL, $object_id, $meta_key, $single );
+
+	if ( NULL !== $check )
+		return ( $single && is_array( $check ) ) ? $check[0] : $check;
+
+	$meta_cache = wp_cache_get( $object_id, $meta_type . '_meta' );
+	// @NOW 019 -> wp-includes/cache.php
+}
