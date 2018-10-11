c8b76a7dca2d944116a33b92351d29bdfe0b9111
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-11 17:15:12 +0900

    Add function: wp_roles() Add file: wp-includes/class-wp-roles.php

--- REF/wp-includes/capabilities.php
+++ REF/wp-includes/capabilities.php
@@ -1,2 +1,25 @@
 <?php
-// @NOW 018
+/**
+ * Core User Role & Capabilities API
+ *
+ * @package    WordPress
+ * @subpackage Users
+ */
+
+/**
+ * Retrieves the global WP_Roles instance and instantiates it if necessary.
+ *
+ * @since  4.3.0
+ * @global WP_Roles $wp_roles WP_Roles global instance.
+ *
+ * @return WP_Roles WP_Roles global instance if not already instantiated.
+ */
+function wp_roles()
+{
+	global $wp_roles;
+
+	if ( ! isset( $wp_roles ) ) {
+		$wp_roles = new WP_Roles();
+// @NOW 018 -> wp-includes/class-wp-roles.php
+	}
+}
