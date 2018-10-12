76c2a4fd27e7572e69312f981eb9a8c093d89f56
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-12 23:45:57 +0900

    Finish function: get_status_header_desc( int $code )

--- REF/wp-includes/functions.php
+++ REF/wp-includes/functions.php
@@ -113,7 +113,90 @@ function is_serialized( $data, $strict = TRUE )
 	return FALSE;
 }
 
-// @NOW 028
+/**
+ * Retrieve the description for the HTTP status.
+ *
+ * @since  2.3.0
+ * @global array $wp_header_to_desc
+ *
+ * @param  int    $code HTTP status code.
+ * @return string Empty string if not found, or description if found.
+ */
+function get_status_header_desc( $code )
+{
+	global $wp_header_to_desc;
+	$code = absint( $code );
+
+	if ( ! isset( $wp_header_to_desc ) ) {
+		$wp_header_to_desc = [
+			100 => 'Continue',
+			101 => 'Switching Protocols',
+			102 => 'Processing',
+
+			200 => 'OK',
+			201 => 'Created',
+			202 => 'Accepted',
+			203 => 'Non-Authoritative Information',
+			204 => 'No Content',
+			205 => 'Reset Content',
+			206 => 'Partial Content',
+			207 => 'Multi-Status',
+			226 => 'IM Used',
+
+			300 => 'Multiple Choices',
+			301 => 'Moved Permanently',
+			302 => 'Found',
+			303 => 'See Other',
+			304 => 'Not Modified',
+			305 => 'Use Proxy',
+			306 => 'Reserved',
+			307 => 'Temporary Redirect',
+			308 => 'Permanant Redirect',
+
+			400 => 'Bad Request',
+			401 => 'Unauthorized',
+			402 => 'Payment Required',
+			403 => 'Forbidden',
+			404 => 'Not Found',
+			405 => 'Method Not Allowed',
+			406 => 'Not Acceptable',
+			407 => 'Proxy Authentication Required',
+			408 => 'Request Timeout',
+			409 => 'Conflict',
+			410 => 'Gone',
+			411 => 'Length Required',
+			412 => 'Precondition Failed',
+			413 => 'Request Entity Too Large',
+			414 => 'Request-URI Too Long',
+			415 => 'Unsupported Media Type',
+			416 => 'Requested Range Not Satisfiable',
+			417 => 'Expectation Failed',
+			418 => 'I\'m a teapot',
+			421 => 'Misdirected Request',
+			422 => 'Unprocessable Entity',
+			423 => 'Locked',
+			424 => 'Failed Dependency',
+			426 => 'Upgrade Required',
+			428 => 'Precondition Required',
+			429 => 'Too Many Requests',
+			431 => 'Request Header Fields Too Large',
+			451 => 'Unavailable For Legal Reasons',
+
+			500 => 'Internal Server Error',
+			501 => 'Not Implemented',
+			502 => 'Bad Gateway',
+			503 => 'Service Unavailable',
+			504 => 'Gateway Timeout',
+			505 => 'HTTP Version Not Supported',
+			506 => 'Variant Also Negotiates',
+			507 => 'Insufficient Storage',
+			510 => 'Not Extended',
+			511 => 'Network Authentication Required'
+		];
+	}
+
+	return isset( $wp_header_to_desc[ $code ] ) ? $wp_header_to_desc[ $code ] : '';
+}
 
 /**
  * Set HTTP status header.
@@ -130,7 +213,7 @@ function status_header( $code, $description = '' )
 {
 	if ( ! $description ) {
 		$description = get_status_header_desc( $code );
-// @NOW 027 -> wp-includes/functions.php
+// @NOW 027
 	}
 }
 
