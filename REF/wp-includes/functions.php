d5c586eaafb12a09c182b8a352c31003898ea89d
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-12 23:24:33 +0900

    Add function: status_header( int $code [, string $description = ''] )

--- REF/wp-includes/functions.php
+++ REF/wp-includes/functions.php
@@ -113,7 +113,26 @@ function is_serialized( $data, $strict = TRUE )
 	return FALSE;
 }
 
-// @NOW 027
+// @NOW 028
+
+/**
+ * Set HTTP status header.
+ *
+ * @since 2.0.0
+ * @since 4.4.0 Added the `$description` parameter.
+ * @see   get_status_header_desc()
+ *
+ * @param int    $code        HTTP status code.
+ * @param string $description Optional.
+ *                            A custom description for the HTTP status.
+ */
+function status_header( $code, $description = '' )
+{
+	if ( ! $description ) {
+		$description = get_status_header_desc( $code );
+// @NOW 027 -> wp-includes/functions.php
+	}
+}
 
 /**
  * Kill WordPress execution and display HTML message with error message.
