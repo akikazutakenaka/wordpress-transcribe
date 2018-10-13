ec41c2c6541b8e17bb03184ccfcc79c6691a547d
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-13 19:04:39 +0900

    Add class: WP_Network_Query

--- ORI/wp-includes/class-wp-network-query.php
+++ ORI/wp-includes/class-wp-network-query.php
@@ -15,69 +15,13 @@
  * @see WP_Network_Query::__construct() for accepted arguments.
  */
 class WP_Network_Query {
-
-	/**
-	 * SQL for database query.
-	 *
-	 * @since 4.6.0
-	 * @var string
-	 */
-	public $request;
-
-	/**
-	 * SQL query clauses.
-	 *
-	 * @since 4.6.0
-	 * @var array
-	 */
-	protected $sql_clauses = array(
-		'select'  => '',
-		'from'    => '',
-		'where'   => array(),
-		'groupby' => '',
-		'orderby' => '',
-		'limits'  => '',
-	);
-
-	/**
-	 * Query vars set by the user.
-	 *
-	 * @since 4.6.0
-	 * @var array
-	 */
-	public $query_vars;
-
-	/**
-	 * Default values for query vars.
-	 *
-	 * @since 4.6.0
-	 * @var array
-	 */
-	public $query_var_defaults;
-
-	/**
-	 * List of networks located by the query.
-	 *
-	 * @since 4.6.0
-	 * @var array
-	 */
-	public $networks;
-
-	/**
-	 * The amount of found networks for the current query.
-	 *
-	 * @since 4.6.0
-	 * @var int
-	 */
-	public $found_networks = 0;
-
-	/**
-	 * The number of pages.
-	 *
-	 * @since 4.6.0
-	 * @var int
-	 */
-	public $max_num_pages = 0;
+	// refactored. public $request;
+	// refactored. protected $sql_clauses = array();
+	// refactored. public $query_vars;
+	// refactored. public $query_var_defaults;
+	// refactored. public $networks;
+	// refactored. public $found_networks = 0;
+	// refactored. public $max_num_pages = 0;
 
 	/**
 	 * Constructor.
--- REF/wp-includes/class-wp-network-query.php
+++ REF/wp-includes/class-wp-network-query.php
@@ -1,2 +1,89 @@
 <?php
+/**
+ * Network API: WP_Network_Query class
+ *
+ * @package    WordPress
+ * @subpackage Multisite
+ * @since      4.6.0
+ */
+
+/**
+ * Core class used for querying networks.
+ *
+ * @since 4.6.0
+ * @see   WP_Network_Query::__construct() for accepted arguments.
+ */
+class WP_Network_Query
+{
+	/**
+	 * SQL for database query.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @var string
+	 */
+	public $request;
+
+	/**
+	 * SQL query clauses.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @var array
+	 */
+	protected $sql_clauses = [
+		'select'  => '',
+		'from'    => '',
+		'where'   => [],
+		'groupby' => '',
+		'orderby' => '',
+		'limits'  => ''
+	];
+
+	/**
+	 * Query vars set by the user.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @var array
+	 */
+	public $query_vars;
+
+	/**
+	 * Default values for query vars.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @var array
+	 */
+	public $query_var_defaults;
+
+	/**
+	 * List of networks located by the query.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @var array
+	 */
+	public $networks;
+
+	/**
+	 * The amount of found networks for the current query.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @var int
+	 */
+	public $found_networks = 0;
+
+	/**
+	 * The number of pages.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @var int
+	 */
+	public $max_num_pages = 0;
+
 // @NOW 024
+}
