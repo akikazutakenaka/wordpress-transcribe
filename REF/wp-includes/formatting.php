a7ab681c778d1cba164abb2bb47f93981c170049
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-13 11:40:11 +0900

    Add function: remove_accents( string $string )

--- REF/wp-includes/formatting.php
+++ REF/wp-includes/formatting.php
@@ -7,6 +7,44 @@
  * @package WordPress
  */
 
+// @NOW 019
+
+/**
+ * Converts all accent characters to ASCII characters.
+ *
+ * If there are no accent characters, then the string given is just returned.
+ *
+ * **Accent characters converted:**
+ *
+ * - Currency signs
+ * - Decompositions for Latin-1 Supplement
+ * - Decompositions for Latin Extended-A
+ * - Decompositions for Latin Extended-B
+ * - Vowels with diacritic (Chinese, Hanyu Pinyin)
+ * - German (`de_DE`), German formal (`de_DE_formal`), German (Switzerland) formal (`de_CH`), and German (Switzerland) informal (`de_CH_informal`) locales
+ * - Danish (`da_DK`) locale
+ * - Catalan (`ca`) locale
+ * - Serbian (`sr_RS`) and Bosinan (`bs_BA`) locales
+ *
+ * @since 1.2.1
+ * @since 4.6.0 Added locale support for `de_CH`, `de_CH_informal`, and `ca`.
+ * @since 4.7.0 Added locale support for `sr_RS`.
+ * @since 4.8.0 Added locale support for `bs_BA`.
+ *
+ * @param  string $string Text that might have accent characters.
+ * @return string Filtered string with replaced "nice" characters.
+ */
+function remove_accents( $string )
+{
+	if ( ! preg_match( '/[\x80-\xFF]/', $string ) ) {
+		return $string;
+	}
+
+	if ( seems_utf8( $string ) ) {
+// @NOW 018 -> wp-includes/formatting.php
+	}
+}
+
 /**
  * Sanitizes a username, stripping out unsafe characters.
  *
@@ -24,7 +62,8 @@ function sanitize_user( $username, $strict = FALSE )
 {
 	$raw_username = $username;
 	$username = wp_strip_all_tags( $username );
-// @NOW 017
+	$username = remove_accents( $username );
+// @NOW 017 -> wp-includes/formatting.php
 }
 
 /**
