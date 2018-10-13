5dc10974b60d3ad5c0ce24cd8402b46c91ebf78d
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-13 11:59:42 +0900

    Finish function: get_site_option( string $option [, mixed $default = FALSE [, bool $deprecated = TRUE]] )

--- ORI/wp-includes/option.php
+++ ORI/wp-includes/option.php
@@ -900,23 +900,7 @@ function delete_all_user_settings() {
 	setcookie( 'wp-settings-' . $user_id, ' ', time() - YEAR_IN_SECONDS, SITECOOKIEPATH );
 }
 
-/**
- * Retrieve an option value for the current network based on name of option.
- *
- * @since 2.8.0
- * @since 4.4.0 The `$use_cache` parameter was deprecated.
- * @since 4.4.0 Modified into wrapper for get_network_option()
- *
- * @see get_network_option()
- *
- * @param string $option     Name of option to retrieve. Expected to not be SQL-escaped.
- * @param mixed  $default    Optional value to return if option doesn't exist. Default false.
- * @param bool   $deprecated Whether to use cache. Multisite only. Always set to true.
- * @return mixed Value set for the option.
- */
-function get_site_option( $option, $default = false, $deprecated = true ) {
-	return get_network_option( null, $option, $default );
-}
+// refactored. function get_site_option( $option, $default = false, $deprecated = true ) {}
 
 /**
  * Add a new option for the current network.
--- REF/wp-includes/option.php
+++ REF/wp-includes/option.php
@@ -226,4 +226,26 @@ EOQ
 	return apply_filters( 'alloptions', $alloptions );
 }
 
+/**
+ * Retrieve an option value for the current network based on name of option.
+ *
+ * @since 2.8.0
+ * @since 4.4.0 The `$use_cache` parameter was deprecated.
+ * @since 4.4.0 Modified into wrapper for get_network_option().
+ * @see   get_network_option()
+ *
+ * @param  string $option     Name of option to retrieve.
+ *                            Expected to not be SQL-escaped.
+ * @param  mixed  $default    Optional value to return if option doesn't exist.
+ *                            Default false.
+ * @param  bool   $deprecated Whether to use cache.
+ *                            Multisite only.
+ *                            Always set to true.
+ * @return mixed  Value set for the option.
+ */
+function get_site_option( $option, $default = FALSE, $deprecated = TRUE )
+{
+	return get_network_option( NULL, $option, $default );
+}
+
 // @NOW 020
