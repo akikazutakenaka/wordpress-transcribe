f76ab50b6f3d8fa3f6907ce1442a1afce4f4b91f
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-13 21:10:12 +0900

    Add function: WP_Network_Query::__construct( [string|array $query = ''] )

--- REF/wp-includes/class-wp-network-query.php
+++ REF/wp-includes/class-wp-network-query.php
@@ -85,5 +85,86 @@ class WP_Network_Query
 	 */
 	public $max_num_pages = 0;
 
-// @NOW 024
+	/**
+	 * Constructor.
+	 *
+	 * Sets up the network query, based on the query vars passed.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @param string|array $query {
+	 *     Optional.
+	 *     Array or query string of network query parameters.
+	 *     Default empty.
+	 *
+	 *     @type array        $network__in          Array of network IDs to include.
+	 *                                              Default empty.
+	 *     @type array        $network__not_in      Array of network IDs to exclude.
+	 *                                              Default empty.
+	 *     @type bool         $count                Whether to return a network count (true) or array of network objects.
+	 *                                              Default false.
+	 *     @type string       $fields               Network fields to return.
+	 *                                              Accepts 'ids' (returns an array of network IDs) or empty (returns an array of complete network objects).
+	 *                                              Default empty.
+	 *     @type int          $number               Maximum number of networks to retrieve.
+	 *                                              Default empty (no limit).
+	 *     @type int          $offset               Number of networks to offset the query.
+	 *                                              Used to build LIMIT clause.
+	 *                                              Default 0.
+	 *     @type bool         $no_found_rows        Whether to disable the `SQL_CALC_FOUND_ROWS` query.
+	 *                                              Default true.
+	 *     @type string|array $orderby              Network status or array of statuses.
+	 *                                              Accepts 'id', 'domain', 'path', 'domain_length', 'path_length' and 'network__in'.
+	 *                                              Also accepts false, an empty array, or 'none' to disable `ORDER BY` clause.
+	 *                                              Default 'id'.
+	 *     @type string       $order                How to order retrieved networks.
+	 *                                              Accepts 'ASC', 'DESC'.
+	 *                                              Default 'ASC'.
+	 *     @type string       $domain               Limit results to those affiliated with a given domain.
+	 *                                              Default empty.
+	 *     @type array        $domain__in           Array of domains to include affiliated networks for.
+	 *                                              Default empty.
+	 *     @type array        $domain__not_in       Array of domains to exclude affiliated networks for.
+	 *                                              Default empty.
+	 *     @type string       $path                 Limit results to those affiliated with a given path.
+	 *                                              Default empty.
+	 *     @type array        $path__in             Array of paths to include affiliated networks for.
+	 *                                              Default empty.
+	 *     @type array        $path__not_in         Array of paths to exclude affiliated networks for.
+	 *                                              Default empty.
+	 *     @type string       $search               Search term(s) to retrieve matching networks for.
+	 *                                              Default empty.
+	 *     @type bool         $update_network_cache Whether to prime the cache for found networks.
+	 *                                              Default true.
+	 * }
+	 */
+	public function __construct( $query = '' )
+	{
+		$this->query_var_defaults = [
+			'network__in'          => '',
+			'network__not_in'      => '',
+			'count'                => FALSE,
+			'fields'               => '',
+			'number'               => '',
+			'offset'               => '',
+			'no_found_rows'        => TRUE,
+			'orderby'              => 'id',
+			'order'                => 'ASC',
+			'domain'               => '',
+			'domain__in'           => '',
+			'domain__not_in'       => '',
+			'path'                 => '',
+			'path__in'             => '',
+			'path__not_in'         => '',
+			'search'               => '',
+			'update_network_cache' => TRUE
+		];
+
+		if ( ! empty( $query ) ) {
+			$this->query( $query );
+// @NOW 024 -> wp-includes/class-wp-network-query.php
+		}
+	}
+
+// @NOW 025
 }
