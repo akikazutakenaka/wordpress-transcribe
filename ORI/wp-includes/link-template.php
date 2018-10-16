db50f908fd751a36e9ab25ecf28b996607b8d72a
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-16 09:30:11 +0900

    Finish function: home_url( [string $path = '' [, string|null $scheme = NULL]] )

--- ORI/wp-includes/link-template.php
+++ ORI/wp-includes/link-template.php
@@ -2899,23 +2899,7 @@ function the_comments_pagination( $args = array() ) {
 	echo get_the_comments_pagination( $args );
 }
 
-/**
- * Retrieves the URL for the current site where the front end is accessible.
- *
- * Returns the 'home' option with the appropriate protocol. The protocol will be 'https'
- * if is_ssl() evaluates to true; otherwise, it will be the same as the 'home' option.
- * If `$scheme` is 'http' or 'https', is_ssl() is overridden.
- *
- * @since 3.0.0
- *
- * @param  string      $path   Optional. Path relative to the home URL. Default empty.
- * @param  string|null $scheme Optional. Scheme to give the home URL context. Accepts
- *                             'http', 'https', 'relative', 'rest', or null. Default null.
- * @return string Home URL link with optional path appended.
- */
-function home_url( $path = '', $scheme = null ) {
-	return get_home_url( null, $path, $scheme );
-}
+// refactored. function home_url( $path = '', $scheme = null ) {}
 
 /**
  * Retrieves the URL for a given site where the front end is accessible.
