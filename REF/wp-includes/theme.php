a969071374f5307637dcfeb190f58787e1cf109f
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-17 10:37:30 +0900

    Add function: get_stylesheet_uri()

--- REF/wp-includes/theme.php
+++ REF/wp-includes/theme.php
@@ -1,2 +1,25 @@
 <?php
-// @NOW 009
+/**
+ * Theme, template, and stylesheet functions.
+ *
+ * @package    WordPress
+ * @subpackage Theme
+ */
+
+// @NOW 010
+
+/**
+ * Retrieves the URI of current theme stylesheet.
+ *
+ * The stylesheet file name is 'style.css' which is appended to the stylesheet directory URI path.
+ * See get_stylesheet_directory_uri().
+ *
+ * @since 1.5.0
+ *
+ * @return string
+ */
+function get_stylesheet_uri()
+{
+	$stylesheet_dir_uri = get_stylesheet_directory_uri();
+// @NOW 009 -> wp-includes/theme.php
+}
