cf3e4e7265ce1427281df3ca92e7f934d0bfd913
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-19 10:02:39 +0900

    Finish function: add_site_option( string $option, mixed $value )

--- ORI/wp-includes/option.php
+++ ORI/wp-includes/option.php
@@ -818,25 +818,7 @@ function delete_all_user_settings() {
 }
 
 // refactored. function get_site_option( $option, $default = false, $deprecated = true ) {}
-
-/**
- * Add a new option for the current network.
- *
- * Existing options will not be updated. Note that prior to 3.3 this wasn't the case.
- *
- * @since 2.8.0
- * @since 4.4.0 Modified into wrapper for add_network_option()
- *
- * @see add_network_option()
- *
- * @param string $option Name of option to add. Expected to not be SQL-escaped.
- * @param mixed  $value  Option value, can be anything. Expected to not be SQL-escaped.
- * @return bool False if the option was not added. True if the option was added.
- */
-function add_site_option( $option, $value ) {
-	return add_network_option( null, $option, $value );
-}
-
+// :
 // refactored. function delete_site_option( $option ) {}
 
 /**
--- REF/wp-includes/option.php
+++ REF/wp-includes/option.php
@@ -352,7 +352,27 @@ function get_site_option( $option, $default = FALSE, $deprecated = TRUE )
 	return get_network_option( NULL, $option, $default );
 }
 
-// @NOW 016
+/**
+ * Add a new option for the current network.
+ *
+ * Existing options will not be updated.
+ * Note that prior to 3.3 this wasn't the case.
+ *
+ * @since 2.8.0
+ * @since 4.4.0 Modified into wrapper for add_network_option().
+ * @see   add_network_option()
+ *
+ * @param  string $option Name of option to add.
+ *                        Expected to not be SQL-escaped.
+ * @param  mixed  $value  Option value, can be anything.
+ *                        Expected to not be SQL-escaped.
+ * @return bool   False if the option was not added.
+ *                True if the option was added.
+ */
+function add_site_option( $option, $value )
+{
+	return add_network_option( NULL, $option, $value );
+}
 
 /**
  * Removes a option by name for the current network.
@@ -502,6 +522,8 @@ EOQ
 	 */
 	return apply_filters( "site_option_{$option}", $value, $option, $network_id );
 }
+
+// @NOW 016
 
 /**
  * Removes a network option by name.
