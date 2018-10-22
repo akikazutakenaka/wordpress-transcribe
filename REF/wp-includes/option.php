811b4d50dbb8f51cbf0b2c08018602b57943e518
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-22 14:16:55 +0900

    Finish function: update_network_option( int $network_id, string $option, mixed $value )

--- REF/wp-includes/option.php
+++ REF/wp-includes/option.php
@@ -979,8 +979,47 @@ function update_network_option( $network_id, $option, $value )
 				'site_id'  => $network_id,
 				'meta_key' => $option
 			) );
-// @NOW 016
+
+		if ( $result ) {
+			$cache_key = "$network_id:$option";
+			wp_cache_set( $cache_key, $value, 'site-options' );
+		}
 	}
+
+	if ( $result ) {
+		/**
+		 * Fires after the value of a specific network option has been successfully updated.
+		 *
+		 * The dynamic portion of the hook name, `$option`, refers to the option name.
+		 *
+		 * @since 2.9.0 As "update_site_option_{$key}"
+		 * @since 3.0.0
+		 * @since 4.7.0 The `$network_id` parameter was added.
+		 *
+		 * @param string $option     Name of the network option.
+		 * @param mixed  $value      Current value of the network option.
+		 * @param mixed  $old_value  Old value of the network option.
+		 * @param int    $network_id ID of the network.
+		 */
+		do_action( "update_site_option_{$option}", $option, $value, $old_value, $network_id );
+
+		/**
+		 * Fires after the value of a network option has been successfully updated.
+		 *
+		 * @since 3.0.0
+		 * @since 4.7.0 The `$network_id` parameter was added.
+		 *
+		 * @param string $option     Name of the network option.
+		 * @param mixed  $value      Current value of the network option.
+		 * @param mixed  $old_value  Old value of the network option.
+		 * @param int    $network_id ID of the network.
+		 */
+		do_action( 'update_site_option', $option, $value, $old_value, $network_id );
+
+		return TRUE;
+	}
+
+	return FALSE;
 }
 
 /**
@@ -1121,7 +1160,7 @@ function set_site_transient( $transient, $value, $expiration = 0 )
 		} else {
 			if ( $expiration ) {
 				update_site_option( $transient_timeout, time() + $expiration );
-// @NOW 015 -> wp-includes/option.php
+// @NOW 015
 			}
 		}
 	}
