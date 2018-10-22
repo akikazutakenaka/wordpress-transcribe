a3fbe7638f039985ec8aa84a58dee6db3fbe9506
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-22 10:23:05 +0900

    Add file: wp-admin/includes/class-walker-category-checklist.php

--- REF/wp-includes/template.php
+++ REF/wp-includes/template.php
@@ -1,2 +1,14 @@
 <?php
-// @NOW 019
+/**
+ * Template WordPress Administration API.
+ *
+ * A Big Mess.
+ * Also some neat functions that are nicely written.
+ *
+ * @package    WordPress
+ * @subpackage Administration
+ */
+
+// Walker_Category_Checklist class
+require_once( ABSPATH . 'wp-admin/includes/class-walker-category-checklist.php' );
+// @NOW 019 -> wp-admin/includes/class-walker-category-checklist.php
