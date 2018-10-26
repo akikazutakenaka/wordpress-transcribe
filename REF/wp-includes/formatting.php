552768fdba9a1e8674b83007d9d7d7c6bfef7089
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-26 09:49:49 +0900

    Add function: format_to_edit( string $content [, bool $rich_text = FALSE] )

--- REF/wp-includes/formatting.php
+++ REF/wp-includes/formatting.php
@@ -1810,7 +1810,37 @@ function force_balance_tags( $text )
 	return $newtext;
 }
 
-// @NOW 010
+/**
+ * Acts on text which is about to be edited.
+ *
+ * The $content is run through esc_textarea(), which uses htmlspecialchars() to convert special characters to HTML entities.
+ * If `$richedit` is set to true, it is simply a holder for the {@see 'format_to_edit'} filter.
+ *
+ * @since 0.71
+ * @since 4.4.0 The `$richedit` parameter was renamed to `$rich_text` for clarity.
+ *
+ * @param  string $content   The text about to be edited.
+ * @param  bool   $rich_text Optional.
+ *                           Whether `$content` should be considered rich text, in which case it would not be passed through esc_textarea().
+ *                           Default false.
+ * @return string The text after the filter (and possibly htmlspecialchars()) has been run.
+ */
+function format_to_edit( $content, $rich_text = FALSE )
+{
+	/**
+	 * Filters the text to be formatted for editing.
+	 *
+	 * @since 1.2.0
+	 *
+	 * @param string $content The text, prior to formatting for editing.
+	 */
+	$content = apply_filters( 'format_to_edit', $content );
+
+	if ( ! $rich_text ) {
+		$content = esc_textarea( $content );
+// @NOW 010 -> wp-includes/formatting.php
+	}
+}
 
 /**
  * Appends a trailing slash.
@@ -2222,6 +2252,8 @@ function esc_html( $text )
 	 */
 	return apply_filters( 'esc_html', $safe_text, $text );
 }
+
+// @NOW 011
 
 /**
  * Sanitises various option values based on the nature of the option.
