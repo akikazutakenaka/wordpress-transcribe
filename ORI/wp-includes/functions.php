f10a747b01e7c3cb2acbda985c17906ebe74d60c
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-29 15:18:30 +0900

    Finish function: is_main_site( [int $site_id = NULL [, int $network_id = NULL]] )

--- ORI/wp-includes/functions.php
+++ ORI/wp-includes/functions.php
@@ -3669,31 +3669,7 @@ function wp_suspend_cache_invalidation( $suspend = true ) {
 	return $current_suspend;
 }
 
-/**
- * Determine whether a site is the main site of the current network.
- *
- * @since 3.0.0
- * @since 4.9.0 The $network_id parameter has been added.
- *
- * @param int $site_id    Optional. Site ID to test. Defaults to current site.
- * @param int $network_id Optional. Network ID of the network to check for.
- *                        Defaults to current network.
- * @return bool True if $site_id is the main site of the network, or if not
- *              running Multisite.
- */
-function is_main_site( $site_id = null, $network_id = null ) {
-	if ( ! is_multisite() ) {
-		return true;
-	}
-
-	if ( ! $site_id ) {
-		$site_id = get_current_blog_id();
-	}
-
-	$site_id = (int) $site_id;
-
-	return $site_id === get_main_site_id( $network_id );
-}
+// refactored. function is_main_site( $site_id = null, $network_id = null ) {}
 
 /**
  * Gets the main site ID.
