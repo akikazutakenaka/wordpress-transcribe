4eb83e93af1722f17a93d05345dd4bf89c36a16c
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-29 12:46:37 +0900

    Finish function: wp_get_upload_dir()

--- ORI/wp-includes/functions.php
+++ ORI/wp-includes/functions.php
@@ -1498,22 +1498,7 @@ function win_is_writable( $path ) {
 	return true;
 }
 
-/**
- * Retrieves uploads directory information.
- *
- * Same as wp_upload_dir() but "light weight" as it doesn't attempt to create the uploads directory.
- * Intended for use in themes, when only 'basedir' and 'baseurl' are needed, generally in all cases
- * when not uploading files.
- *
- * @since 4.5.0
- *
- * @see wp_upload_dir()
- *
- * @return array See wp_upload_dir() for description.
- */
-function wp_get_upload_dir() {
-	return wp_upload_dir( null, false );
-}
+// refactored. function wp_get_upload_dir() {}
 
 /**
  * Get an array containing the current upload directory's path and url.
--- REF/wp-includes/functions.php
+++ REF/wp-includes/functions.php
@@ -376,6 +376,22 @@ function wp_normalize_path( $path )
 	return $wrapper . $path;
 }
 
+/**
+ * Retrieves uploads directory information.
+ *
+ * Same as wp_upload_dir() but "light weight" as it doesn't attempt to create the uploads directory.
+ * Intended for use in themes, when only 'basedir' and 'baseurl' are needed, generally in all cases when not uploading files.
+ *
+ * @since 4.5.0
+ * @see   wp_upload_dir()
+ *
+ * @return array See wp_upload_dir() for description.
+ */
+function wp_get_upload_dir()
+{
+	return wp_upload_dir( NULL, FALSE );
+}
+
 // @NOW 013
 
 /**
