f0e3a3d66ecce59498d80b3c666172c4cf42d8d4
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-29 13:02:27 +0900

    Add function: wp_upload_dir( [string $time = NULL [, bool $create_dir = TRUE [, bool $refresh_cache = FALSE]]] )

--- REF/wp-includes/functions.php
+++ REF/wp-includes/functions.php
@@ -392,7 +392,57 @@ function wp_get_upload_dir()
 	return wp_upload_dir( NULL, FALSE );
 }
 
-// @NOW 013
+/**
+ * Get an array containing the current upload directory's path and url.
+ *
+ * Checks the 'upload_path' option, which should be from the web root folder, and if it isn't empty it will be used.
+ * If it is empty, then the path will be 'WP_CONTENT_DIR/uploads'.
+ * If the 'UPLOADS' constant is defined, then it will override the 'upload_path' option and 'WP_CONTENT_DIR/uploads' path.
+ *
+ * The upload URL path is set either by the 'upload_url_path' option or by using the 'WP_CONTENT_URL' constant and appending '/uploads' to the path.
+ *
+ * If the 'uploads_use_yearmonth_folders' is set to true (checkbox if checked in the administration settings panel), then the time will be used.
+ * The format will be year first and then month.
+ *
+ * If the path couldn't be created, then an error will be returned with the key 'error' containing the error message.
+ * The error suggests that the parent directory is not writable by the server.
+ *
+ * On success, the returned array will have many indices:
+ * 'path' - base directory and sub directory or full path to upload directory.
+ * 'url' - base url and sub directory or absolute URL to upload directory.
+ * 'subdir' - sub directory if uploads use year/month folders option is on.
+ * 'basedir' - path without subdir.
+ * 'baseurl' - URL path without subdir.
+ * 'error' - false or error message.
+ *
+ * @since     2.0.0
+ * @uses      _wp_upload_dir()
+ * @staticvar array $cache
+ * @staticvar array $tested_paths
+ *
+ * @param  string $time          Optional.
+ *                               Time formatted in 'yyyy/mm'.
+ *                               Default null.
+ * @param  bool   $create_dir    Optional.
+ *                               Whether to check and create the uploads directory.
+ *                               Default true for backward compatibility.
+ * @param  bool   $refresh_cache Optional.
+ *                               Whether to refresh the cache.
+ *                               Default false.
+ * @return array  See above for description.
+ */
+function wp_upload_dir( $time = NULL, $create_dir = TRUE, $refresh_cache = FALSE )
+{
+	static $cache = array(), $tested_paths = array();
+	$key = sprintf( '%d-%s', get_current_blog_id(), ( string ) $time );
+
+	if ( $refresh_cache || empty( $cache[ $key ] ) ) {
+		$cache[ $key ] = _wp_upload_dir( $time );
+// @NOW 013 -> wp-includes/functions.php
+	}
+}
+
+// @NOW 014
 
 /**
  * Kill WordPress execution and display HTML message with error message.
