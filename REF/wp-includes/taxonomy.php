c20f682b8f9f306bf1366d2d06cdc32ef1bdcd5d
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-29 11:24:24 +0900

    Add function: is_object_in_taxonomy( string $object_type, string $taxonomy )

--- REF/wp-includes/taxonomy.php
+++ REF/wp-includes/taxonomy.php
@@ -6,4 +6,19 @@
  * @subpackage Taxonomy
  */
 
-// @NOW 009
+// @NOW 010
+
+/**
+* Determine if the given object type is associated with the given taxonomy.
+*
+* @since 3.0.0
+*
+* @param  string $object_type Object type string.
+* @param  string $taxonomy    Single taxonomy name.
+* @return bool   True if object is associated with the taxonomy, otherwise false.
+*/
+function is_object_in_taxonomy( $object_type, $taxonomy )
+{
+	$taxonomies = get_object_taxonomies( $object_type );
+// @NOW 009 -> wp-includes/taxonomy.php
+}
