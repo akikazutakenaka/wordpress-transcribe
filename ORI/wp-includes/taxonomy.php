1107bcb7566d88ffe5b1a40e80295637daec7bb2
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-30 10:27:23 +0900

    Finish function: taxonomy_exists( string $taxonomy )

--- ORI/wp-includes/taxonomy.php
+++ ORI/wp-includes/taxonomy.php
@@ -198,23 +198,7 @@ function get_taxonomy( $taxonomy ) {
 	return $wp_taxonomies[$taxonomy];
 }
 
-/**
- * Checks that the taxonomy name exists.
- *
- * Formerly is_taxonomy(), introduced in 2.3.0.
- *
- * @since 3.0.0
- *
- * @global array $wp_taxonomies The registered taxonomies.
- *
- * @param string $taxonomy Name of taxonomy object.
- * @return bool Whether the taxonomy exists.
- */
-function taxonomy_exists( $taxonomy ) {
-	global $wp_taxonomies;
-
-	return isset( $wp_taxonomies[$taxonomy] );
-}
+// refactored. function taxonomy_exists( $taxonomy ) {}
 
 /**
  * Whether the taxonomy object is hierarchical.
--- REF/wp-includes/taxonomy.php
+++ REF/wp-includes/taxonomy.php
@@ -55,7 +55,22 @@ function get_object_taxonomies( $object, $output = 'names' )
 	return $taxonomies;
 }
 
-// self -> @NOW 012
+/**
+ * Checks that the taxonomy name exists.
+ *
+ * Formerly is_taxonomy(), introduced in 2.3.0.
+ *
+ * @since  3.0.0
+ * @global array $wp_taxonomies The registered taxonomies.
+ *
+ * @param  string $taxonomy Name of taxonomy object.
+ * @return bool   Whether the taxonomy exists.
+ */
+function taxonomy_exists( $taxonomy )
+{
+	global $wp_taxonomies;
+	return isset( $wp_taxonomies[ $taxonomy ] );
+}
 
 /**
  * Get all Term data from database by Term ID.
@@ -108,7 +123,7 @@ function get_term( $term, $taxonomy = '', $output = OBJECT, $filter = 'raw' )
 	}
 
 	if ( $taxonomy && ! taxonomy_exists( $taxonomy ) ) {
-// self -> @NOW 011 -> self
+// self -> @NOW 011
 	}
 }
 
