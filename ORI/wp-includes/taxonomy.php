f289334fb9bdf47158c507894ed46f4a3c423951
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-30 09:37:10 +0900

    Finish function: update_termmeta_cache( array $term_ids )

--- ORI/wp-includes/taxonomy.php
+++ ORI/wp-includes/taxonomy.php
@@ -1193,25 +1193,7 @@ function update_term_meta( $term_id, $meta_key, $meta_value, $prev_value = '' ) 
 	return $updated;
 }
 
-/**
- * Updates metadata cache for list of term IDs.
- *
- * Performs SQL query to retrieve all metadata for the terms matching `$term_ids` and stores them in the cache.
- * Subsequent calls to `get_term_meta()` will not need to query the database.
- *
- * @since 4.4.0
- *
- * @param array $term_ids List of term IDs.
- * @return array|false Returns false if there is nothing to update. Returns an array of metadata on success.
- */
-function update_termmeta_cache( $term_ids ) {
-	// Bail if term meta table is not installed.
-	if ( get_option( 'db_version' ) < 34370 ) {
-		return;
-	}
-
-	return update_meta_cache( 'term', $term_ids );
-}
+// refactored. function update_termmeta_cache( $term_ids ) {}
 
 /**
  * Get all meta data, including meta IDs, for the given term ID.
--- REF/wp-includes/taxonomy.php
+++ REF/wp-includes/taxonomy.php
@@ -55,7 +55,27 @@ function get_object_taxonomies( $object, $output = 'names' )
 	return $taxonomies;
 }
 
-// self -> @NOW 011
+/**
+ * Updates metadata cache for list of term IDs.
+ *
+ * Performs SQL query to retrieve all metadata for the terms matching `$term_ids` and stores them in the cache.
+ * Subsequent calls to `get_term_meta()` will not need to query the database.
+ *
+ * @since 4.4.0
+ *
+ * @param  array       $term_ids List of term IDs.
+ * @return array|false Returns false if there is nothing to update.
+ *                     Returns an array of metadata on success.
+ */
+function update_termmeta_cache( $term_ids )
+{
+	// Bail if term meta table is not installed.
+	if ( get_option( 'db_version' ) < 34370 ) {
+		return;
+	}
+
+	return update_meta_cache( 'term', $term_ids );
+}
 
 /**
  * Retrieves the taxonomy relationship to the term object id.
@@ -94,7 +114,7 @@ function get_object_term_cache( $id, $taxonomy )
 
 	// Fill the term objects.
 	_prime_term_caches( $term_ids );
-// wp-includes/category-template.php -> @NOW 010 -> self
+// wp-includes/category-template.php -> @NOW 010
 }
 
 /**
