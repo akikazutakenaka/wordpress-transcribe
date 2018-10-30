ae5e852afe9c6692aa0109d90c0cd0a3745e3019
竹中彰一 <akikazu.takenaka@gmail.com>
2018-10-31 02:57:35 +0900

    Add class: WP_Term

--- ORI/wp-includes/class-wp-term.php
+++ ORI/wp-includes/class-wp-term.php
@@ -15,88 +15,9 @@
  * @property-read object $data Sanitized term data.
  */
 final class WP_Term {
-
-	/**
-	 * Term ID.
-	 *
-	 * @since 4.4.0
-	 * @var int
-	 */
-	public $term_id;
-
-	/**
-	 * The term's name.
-	 *
-	 * @since 4.4.0
-	 * @var string
-	 */
-	public $name = '';
-
-	/**
-	 * The term's slug.
-	 *
-	 * @since 4.4.0
-	 * @var string
-	 */
-	public $slug = '';
-
-	/**
-	 * The term's term_group.
-	 *
-	 * @since 4.4.0
-	 * @var string
-	 */
-	public $term_group = '';
-
-	/**
-	 * Term Taxonomy ID.
-	 *
-	 * @since 4.4.0
-	 * @var int
-	 */
-	public $term_taxonomy_id = 0;
-
-	/**
-	 * The term's taxonomy name.
-	 *
-	 * @since 4.4.0
-	 * @var string
-	 */
-	public $taxonomy = '';
-
-	/**
-	 * The term's description.
-	 *
-	 * @since 4.4.0
-	 * @var string
-	 */
-	public $description = '';
-
-	/**
-	 * ID of a term's parent term.
-	 *
-	 * @since 4.4.0
-	 * @var int
-	 */
-	public $parent = 0;
-
-	/**
-	 * Cached object count for this term.
-	 *
-	 * @since 4.4.0
-	 * @var int
-	 */
-	public $count = 0;
-
-	/**
-	 * Stores the term object's sanitization level.
-	 *
-	 * Does not correspond to a database field.
-	 *
-	 * @since 4.4.0
-	 * @var string
-	 */
-	public $filter = 'raw';
+	// refactored. public $term_id;
+	// :
+	// refactored. public $filter = 'raw';
 
 	/**
 	 * Retrieve WP_Term instance.
--- REF/wp-includes/class-wp-term.php
+++ REF/wp-includes/class-wp-term.php
@@ -7,4 +7,104 @@
  * @since      4.4.0
  */
 
+/**
+ * Core class used to implement the WP_Term object.
+ *
+ * @since 4.4.0
+ */
+final class WP_Term
+{
+	/**
+	 * Term ID.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var int
+	 */
+	public $term_id;
+
+	/**
+	 * The term's name.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var string
+	 */
+	public $name = '';
+
+	/**
+	 * The term's slug.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var string
+	 */
+	public $slug = '';
+
+	/**
+	 * The term's term_group.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var string
+	 */
+	public $term_group = '';
+
+	/**
+	 * Term Taxonomy ID.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var int
+	 */
+	public $term_taxonomy_id = 0;
+
+	/**
+	 * The term's taxonomy name.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var string
+	 */
+	public $taxonomy = '';
+
+	/**
+	 * The term's description.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var string
+	 */
+	public $description = '';
+
+	/**
+	 * ID of a term's parent term.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var int
+	 */
+	public $parent = 0;
+
+	/**
+	 * Cached object count for this term.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var int
+	 */
+	public $count = 0;
+
+	/**
+	 * Stores the term object's sanitization level.
+	 *
+	 * Does not corespond to a database field.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @var string
+	 */
+	public $filter = 'raw';
+
 // wp-includes/taxonomy.php -> @NOW 012
+}
