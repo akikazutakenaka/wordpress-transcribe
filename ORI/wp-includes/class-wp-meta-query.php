3b785cd2f8758b2375028492427f919c7a36f860
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-10-31 16:16:22 +0900

    Add class: WP_Meta_Query

--- ORI/wp-includes/class-wp-meta-query.php
+++ ORI/wp-includes/class-wp-meta-query.php
@@ -20,79 +20,9 @@
  * @since 3.2.0
  */
 class WP_Meta_Query {
-	/**
-	 * Array of metadata queries.
-	 *
-	 * See WP_Meta_Query::__construct() for information on meta query arguments.
-	 *
-	 * @since 3.2.0
-	 * @var array
-	 */
-	public $queries = array();
-
-	/**
-	 * The relation between the queries. Can be one of 'AND' or 'OR'.
-	 *
-	 * @since 3.2.0
-	 * @var string
-	 */
-	public $relation;
-
-	/**
-	 * Database table to query for the metadata.
-	 *
-	 * @since 4.1.0
-	 * @var string
-	 */
-	public $meta_table;
-
-	/**
-	 * Column in meta_table that represents the ID of the object the metadata belongs to.
-	 *
-	 * @since 4.1.0
-	 * @var string
-	 */
-	public $meta_id_column;
-
-	/**
-	 * Database table that where the metadata's objects are stored (eg $wpdb->users).
-	 *
-	 * @since 4.1.0
-	 * @var string
-	 */
-	public $primary_table;
-
-	/**
-	 * Column in primary_table that represents the ID of the object.
-	 *
-	 * @since 4.1.0
-	 * @var string
-	 */
-	public $primary_id_column;
-
-	/**
-	 * A flat list of table aliases used in JOIN clauses.
-	 *
-	 * @since 4.1.0
-	 * @var array
-	 */
-	protected $table_aliases = array();
-
-	/**
-	 * A flat list of clauses, keyed by clause 'name'.
-	 *
-	 * @since 4.2.0
-	 * @var array
-	 */
-	protected $clauses = array();
-
-	/**
-	 * Whether the query contains any OR relations.
-	 *
-	 * @since 4.3.0
-	 * @var bool
-	 */
-	protected $has_or_relation = false;
+	// refactored. public $queries = array();
+	// :
+	// refactored. protected $has_or_relation = false;
 
 	/**
 	 * Constructor.
--- REF/wp-includes/class-wp-meta-query.php
+++ REF/wp-includes/class-wp-meta-query.php
@@ -7,4 +7,100 @@
  * @since      4.4.0
  */
 
+/**
+ * Core class used to implement meta queries for the Meta API.
+ *
+ * Used for generating SQL clauses that filter a primary query according to metadata keys and values.
+ *
+ * WP_Meta_Query is a helper that allows primary query classes, such as WP_Query and WP_User_Query, to filter their results by object metadata, by generating `JOIN` and `WHERE` subclauses to be attached to the primary SQL query string.
+ *
+ * @since 3.2.0
+ */
+class WP_Meta_Query
+{
+	/**
+	 * Array of metadata queries.
+	 *
+	 * See WP_Meta_Query::__construct() for information on meta query arguments.
+	 *
+	 * @since 3.2.0
+	 *
+	 * @var array
+	 */
+	public $queries = array();
+
+	/**
+	 * The relation between the queries.
+	 * Can be one of 'AND' or 'OR'.
+	 *
+	 * @since 3.2.0
+	 *
+	 * @var string
+	 */
+	public $relation;
+
+	/**
+	 * Database table to query for the metadata.
+	 *
+	 * @since 4.1.0
+	 *
+	 * @var string
+	 */
+	public $meta_table;
+
+	/**
+	 * Column in meta_table that represents the ID of the object the metadata belongs to.
+	 *
+	 * @since 4.1.0
+	 *
+	 * @var string
+	 */
+	public $meta_id_column;
+
+	/**
+	 * Database table that where the metadata's objects are stored (e.g. $wpdb->users).
+	 *
+	 * @since 4.1.0
+	 *
+	 * @var string
+	 */
+	public $primary_table;
+
+	/**
+	 * Column in primary_table that represents the ID of the object.
+	 *
+	 * @since 4.1.0
+	 *
+	 * @var string
+	 */
+	public $primary_id_column;
+
+	/**
+	 * A flat list of table aliases used in JOIN clauses.
+	 *
+	 * @since 4.1.0
+	 *
+	 * @var array
+	 */
+	protected $table_aliases = array();
+
+	/**
+	 * A flat list of clauses, keyed by clause 'name'.
+	 *
+	 * @since 4.2.0
+	 *
+	 * @var array
+	 */
+	protected $clauses = array();
+
+	/**
+	 * Whether the query contains any OR relations.
+	 *
+	 * @since 4.3.0
+	 *
+	 * @var bool
+	 */
+	protected $has_or_relation = FALSE;
+
 // wp-includes/class-wp-term-query.php -> @NOW 013
+}
