af1de635e5c472f0679efa09bfa265bda03778e8
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-02 09:41:02 +0900

    Finish function: WP_Meta_Query::get_sql_for_clause( &array $clause, array $parent_query [, string $clause_key = ''] )

--- REF/wp-includes/class-wp-meta-query.php
+++ REF/wp-includes/class-wp-meta-query.php
@@ -391,7 +391,7 @@ class WP_Meta_Query
 				if ( $this->is_first_order_clause( $clause ) ) {
 					// This is a first-order clause.
 					$clause_sql = $this->get_sql_for_clause( $clause, $query, $key );
-// self -> @NOW 017 -> self
+// self -> @NOW 017
 				}
 			}
 		}
@@ -486,7 +486,97 @@ class WP_Meta_Query
 			: '';
 
 		$meta_type = $this->get_cast_for_type( $_meta_type );
-// self -> @NOW 018
+		$clause['cast'] = $meta_type;
+
+		/**
+		 * Fallback for clause keys is the table alias.
+		 * Key must be a string.
+		 */
+		if ( is_int( $clause_key ) || ! $clause_key ) {
+			$clause_key = $clause['alias'];
+		}
+
+		// Ensure unique clause keys, so none are overwritten.
+		$iterator = 1;
+		$clause_key_base = $clause_key;
+
+		while ( isset( $this->clauses[ $clause_key ] ) ) {
+			$clause_key = $clause_key_base . '-' . $iterator;
+			$iterator++;
+		}
+
+		// Store the clause in our flat array.
+		$this->clauses[ $clause_key ] = &$clause;
+
+		// Next, build the WHERE clause.
+
+		// meta_key.
+		if ( array_key_exists( 'key', $clause ) ) {
+			$sql_chunks['where'][] = 'NOT EXISTS' === $meta_compare
+				? $alias . '.' . $this->meta_id_column . ' IS NULL'
+				: $wpdb->prepare( "$alias.meta_key = %s", trim( $clause['key'] ) );
+		}
+
+		// meta_value.
+		if ( array_key_exists( 'value', $clause ) ) {
+			$meta_value = $clause['value'];
+
+			if ( in_array( $meta_compare, array( 'IN', 'NOT IN', 'BETWEEN', 'NOT BETWEEN' ) ) ) {
+				if ( ! is_array( $meta_value ) ) {
+					$meta_value = preg_split( '/[,\s]+/', $meta_value );
+				}
+			} else {
+				$meta_value = trim( $meta_value );
+			}
+
+			switch ( $meta_compare ) {
+				case 'IN':
+				case 'NOT IN':
+					$meta_compare_string = '(' . substr( str_repeat( ',%s', count( $meta_value ) ), 1 ) . ')';
+					$where = $wpdb->prepare( $meta_compare_string, $meta_value );
+					break;
+
+				case 'BETWEEN':
+				case 'NOT BETWEEN':
+					$meta_value = array_slice( $meta_value, 0, 2 );
+					$where = $wpdb->prepare( '%s AND %s', $meta_value );
+					break;
+
+				case 'LIKE':
+				case 'NOT LIKE':
+					$meta_value = '%' . $wpdb->esc_like( $meta_value ) . '%';
+					$where = $wpdb->prepare( '%s', $meta_value );
+					break;
+
+				// EXISTS with a value is interpreted as '='.
+				case 'EXISTS':
+					$meta_compare = '=';
+					$where = $wpdb->prepare( '%s', $meta_value );
+					break;
+
+				// 'value' is ignored for NOT EXISTS.
+				case 'NOT EXISTS':
+					$where = '';
+					break;
+
+				default:
+					$where = $wpdb->prepare( '%s', $meta_value );
+					break;
+			}
+
+			if ( $where ) {
+				$sql_chunks['where'][] = 'CHAR' === $meta_type
+					? "$alias.meta_value {$meta_compare} {$where}"
+					: "CAST($alias.meta_value AS {$meta_type}) {$meta_compare} {$where}";
+			}
+		}
+
+		// Multiple WHERE clauses (for meta_key and meta_value) should be joined in parentheses.
+		if ( 1 < count( $sql_chunks['where'] ) ) {
+			$sql_chunks['where'] = array( '( ' . implode( ' AND ', $sql_chunks['where'] ) . ' )' );
+		}
+
+		return $sql_chunks;
 	}
 
 	/**
