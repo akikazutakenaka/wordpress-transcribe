2cdf5c57806c47044fd303ded5caac8f632d1432
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-05 15:19:20 +0900

    Finish function: get_the_terms( int|object $post, string $taxonomy )

--- ORI/wp-includes/category-template.php
+++ ORI/wp-includes/category-template.php
@@ -1170,45 +1170,7 @@ function term_description( $term = 0, $deprecated = null ) {
 	return is_wp_error( $description ) ? '' : $description;
 }
 
-/**
- * Retrieve the terms of the taxonomy that are attached to the post.
- *
- * @since 2.5.0
- *
- * @param int|object $post Post ID or object.
- * @param string $taxonomy Taxonomy name.
- * @return array|false|WP_Error Array of WP_Term objects on success, false if there are no terms
- *                              or the post does not exist, WP_Error on failure.
- */
-function get_the_terms( $post, $taxonomy ) {
-	if ( ! $post = get_post( $post ) )
-		return false;
-
-	$terms = get_object_term_cache( $post->ID, $taxonomy );
-	if ( false === $terms ) {
-		$terms = wp_get_object_terms( $post->ID, $taxonomy );
-		if ( ! is_wp_error( $terms ) ) {
-			$term_ids = wp_list_pluck( $terms, 'term_id' );
-			wp_cache_add( $post->ID, $term_ids, $taxonomy . '_relationships' );
-		}
-	}
-
-	/**
-	 * Filters the list of terms attached to the given post.
-	 *
-	 * @since 3.1.0
-	 *
-	 * @param array|WP_Error $terms    List of attached terms, or WP_Error on failure.
-	 * @param int            $post_id  Post ID.
-	 * @param string         $taxonomy Name of the taxonomy.
-	 */
-	$terms = apply_filters( 'get_the_terms', $terms, $post->ID, $taxonomy );
-
-	if ( empty( $terms ) )
-		return false;
-
-	return $terms;
-}
+// refactored. function get_the_terms( $post, $taxonomy ) {}
 
 /**
  * Retrieve a post's terms as a list with specified format.
--- REF/wp-includes/category-template.php
+++ REF/wp-includes/category-template.php
@@ -26,16 +26,25 @@ function get_the_terms( $post, $taxonomy )
 
 	if ( FALSE === $terms ) {
 		$terms = wp_get_object_terms( $post->ID, $taxonomy );
-/**
- * <- wp-blog-header.php
- * <- wp-load.php
- * <- wp-settings.php
- * <- wp-includes/default-filters.php
- * <- wp-includes/post.php
- * <- wp-includes/post.php
- * <- wp-includes/class-wp-post.php
- * <- wp-includes/class-wp-post.php
- * @NOW 009: wp-includes/category-template.php
- */
+
+		if ( ! is_wp_error( $terms ) ) {
+			$term_ids = wp_list_pluck( $terms, 'term_id' );
+			wp_cache_add( $post->ID, $term_ids, $taxonomy . '_relationships' );
+		}
 	}
+
+	/**
+	 * Filters the list of terms attached to the given post.
+	 *
+	 * @since 3.1.0
+	 *
+	 * @param array|WP_Error $terms    List of attached terms, or WP_Error on failure.
+	 * @param int            $post_id  Post ID.
+	 * @param string         $taxonomy Name of the taxonomy.
+	 */
+	$terms = apply_filters( 'get_the_terms', $terms, $post->ID, $taxonomy );
+
+	return empty( $terms )
+		? FALSE
+		: $terms;
 }
