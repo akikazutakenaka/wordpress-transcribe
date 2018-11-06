4d530f036f8844eb3836044d936ca32043718da3
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-06 14:47:24 +0900

    Add function: get_comment( [&WP_Comment|&string|&int $comment = NULL [, string $output = OBJECT]] ) Add file: wp-includes/class-wp-comment.php

---
+++ REF/wp-includes/class-wp-comment.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Comment API: WP_Comment class
+ *
+ * @package    WordPress
+ * @subpackage Comments
+ * @since      4.4.0
+ */
+
+/**
+ * <- wp-blog-header.php
+ * <- wp-load.php
+ * <- wp-settings.php
+ * <- wp-includes/default-filters.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/class-wp-user.php
+ * <- wp-includes/capabilities.php
+ * <- wp-includes/meta.php
+ * <- wp-includes/comment.php
+ * @NOW 011: wp-includes/class-wp-comment.php
+ */
--- REF/wp-includes/comment.php
+++ REF/wp-includes/comment.php
@@ -1,5 +1,39 @@
 <?php
 /**
+ * Core Comment API
+ *
+ * @package    WordPress
+ * @subpackage Comment
+ */
+
+/**
+ * Retrieves comment data given a comment ID or comment object.
+ *
+ * If an object is passed then the comment data will be cached and then returned after being passed through a filter.
+ * If the comment is empty, then the global comment variable will be used, if it is set.
+ *
+ * @since  2.0.0
+ * @global WP_Comment $comment
+ *
+ * @param  WP_Comment|string|int $comment Comment to retrieve.
+ * @param  string                $output  Optional.
+ *                                        The required return type.
+ *                                        One of OBJECT, ARRAY_A, or ARRAY_N, which correspond to a WP_Comment object, an associative array, or a numeric array, respectively.
+ *                                        Default OBJECT.
+ * @return WP_Comment|array|null Depends on $output value.
+ */
+function get_comment( &$comment = NULL, $output = OBJECT )
+{
+	if ( empty( $comment ) && isset( $GLOBALS['comment'] ) ) {
+		$comment = $GLOBALS['comment'];
+	}
+
+	$_comment = $comment instanceof WP_Comment
+		? $comment
+		: ( is_object( $comment )
+			? new WP_Comment( $comment )
+			: WP_Comment::get_instance( $comment ) );
+/**
  * <- wp-blog-header.php
  * <- wp-load.php
  * <- wp-settings.php
@@ -10,4 +44,6 @@
  * <- wp-includes/capabilities.php
  * <- wp-includes/meta.php
  * @NOW 010: wp-includes/comment.php
+ * -> wp-includes/class-wp-comment.php
  */
+}
