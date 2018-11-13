d5ab4eed929c9fbd1d2e71873643ece9f0642426
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-13 12:34:14 +0900

    Add function: WP_Tax_Query::__construct( array $tax_query )

--- REF/wp-includes/class-wp-tax-query.php
+++ REF/wp-includes/class-wp-tax-query.php
@@ -90,6 +90,43 @@ class WP_Tax_Query
 	 */
 	public $primary_id_column;
 
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.1.0
+	 * @since 4.1.0 Added support for `$operator` 'NOT EXISTS' and 'EXISTS' values.
+	 *
+	 * @param array $tax_query {
+	 *     Array of taxonomy query clauses.
+	 *
+	 *     @type string $relation Optional.
+	 *                            The MySQL keyword used to join the clauses of the query.
+	 *                            Accepts 'AND', or 'OR'.
+	 *                            Default 'AND'.
+	 *     @type array {
+	 *         Optional.
+	 *         An array of first-order clause parameters, or another fully-formed tax query.
+	 *
+	 *         @type string           $taxonomy         Taxonomy being queried.
+	 *                                                  Optional when field=term_taxonomy_id.
+	 *         @type string|int|array $terms            Term or terms to filter by.
+	 *         @type string           $field            Field to match $terms against.
+	 *                                                  Accepts 'term_id', 'slug', 'name', or 'term_taxonomy_id'.
+	 *                                                  Default: 'term_id'.
+	 *         @type string           $operator         MySQL operator to be used with $terms in the WHERE clause.
+	 *                                                  Accepts 'AND', 'IN', 'NOT IN', 'EXISTS', 'NOT EXISTS'.
+	 *         @type bool             $include_children Optional.
+	 *                                                  Whether to include child terms.
+	 *                                                  Requires a $taxonomy.
+	 *                                                  Default: true.
+	 *     }
+	 * }
+	 */
+	public function __construct( $tax_query )
+	{
+		$this->relation = isset( $tax_query['relation'] )
+			? $this->sanitize_relation( $tax_query['relation'] )
+			: 'AND';
 /**
  * <- wp-blog-header.php
  * <- wp-load.php
@@ -103,5 +140,23 @@ class WP_Tax_Query
  * <- wp-includes/class-wp-query.php
  * <- wp-includes/class-wp-query.php
  * @NOW 012: wp-includes/class-wp-tax-query.php
+ * -> wp-includes/class-wp-tax-query.php
+ */
+	}
+
+/**
+ * <- wp-blog-header.php
+ * <- wp-load.php
+ * <- wp-settings.php
+ * <- wp-includes/default-filters.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/class-wp-query.php
+ * <- wp-includes/class-wp-query.php
+ * <- wp-includes/class-wp-query.php
+ * <- wp-includes/class-wp-tax-query.php
+ * @NOW 013: wp-includes/class-wp-tax-query.php
  */
 }
