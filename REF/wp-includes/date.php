fd2999d5723ee8dab21ead6eb6a1babffa2de0af
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-14 11:21:36 +0900

    Add function: WP_Date_Query::get_sql_for_query( array $query [, int $depth = 0] )

--- REF/wp-includes/date.php
+++ REF/wp-includes/date.php
@@ -580,6 +580,48 @@ class WP_Date_Query
  */
 	}
 
+	/**
+	 * Generate SQL clauses for a single query array.
+	 *
+	 * If nested subqueries are found, this method recurses the tree to produce the properly nested SQL.
+	 *
+	 * @since 4.1.0
+	 *
+	 * @param  array $query Query to parse.
+	 * @param  int   $depth Optional.
+	 *                      Number of tree levels deep we currently are.
+	 *                      Used to calculate indentation.
+	 *                      Default 0.
+	 * @return array {
+	 *     Array containing JOIN and WHERE SQL clauses to append to the single query query.
+	 *
+	 *     @type string $join  SQL fragment to append to the main JOIN clause.
+	 *     @type string $where SQL fragment to append to the main WHERE clause.
+	 * }
+	 */
+	protected function get_sql_for_query( $query, $depth = 0 )
+	{
+		$sql_chunks = array(
+			'join'  => array(),
+			'where' => array()
+		);
+		$sql = array(
+			'join'  => '',
+			'where' => ''
+		);
+		$indent = '';
+
+		for ( $i = 0; $i < $depth; $i++ ) {
+			$indent .= "  ";
+		}
+
+		foreach ( $query as $key => $clause ) {
+			if ( 'relation' === $key ) {
+				$relation = $query['relation'];
+			} elseif ( is_array( $clause ) ) {
+				if ( $this->is_first_order_clause( $clause ) ) {
+					// This is a first-order clause.
+					$clause_sql = $this->get_sql_for_clause( $clause, $query );
 /**
  * <- wp-blog-header.php
  * <- wp-load.php
@@ -593,5 +635,26 @@ class WP_Date_Query
  * <- wp-includes/date.php
  * <- wp-includes/date.php
  * @NOW 012: wp-includes/date.php
+ * -> wp-includes/date.php
+ */
+				}
+			}
+		}
+	}
+
+/**
+ * <- wp-blog-header.php
+ * <- wp-load.php
+ * <- wp-settings.php
+ * <- wp-includes/default-filters.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/class-wp-query.php
+ * <- wp-includes/date.php
+ * <- wp-includes/date.php
+ * <- wp-includes/date.php
+ * @NOW 013: wp-includes/date.php
  */
 }
