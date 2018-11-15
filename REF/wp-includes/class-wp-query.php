f0891de4903f556cd058cc0d08272396144cef4e
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-15 15:15:10 +0900

    Finish function: WP_Query::parse_orderby( string $orderby )

--- REF/wp-includes/class-wp-query.php
+++ REF/wp-includes/class-wp-query.php
@@ -1493,6 +1493,101 @@ class WP_Query
 	}
 
 	/**
+	 * Converts the given orderby alias (if allowed) to a properly-prefixed value.
+	 *
+	 * @since  4.0.0
+	 * @global wpdb $wpdb WordPress database abstraction object.
+	 *
+	 * @param  string       $orderby Alias for the field to order by.
+	 * @return string|false Table-prefixed value to used in the ORDER clause.
+	 *                      False otherwise.
+	 */
+	protected function parse_orderby( $orderby )
+	{
+		global $wpdb;
+
+		// Used to filter values.
+		$allowed_keys = array( 'post_name', 'post_author', 'post_date', 'post_title', 'post_modified', 'post_parent', 'post_type', 'name', 'author', 'date', 'title', 'modified', 'parent', 'type', 'ID', 'menu_order', 'comment_count', 'rand' );
+
+		$primary_meta_key = '';
+		$primary_meta_query = FALSE;
+		$meta_clauses = $this->meta_query = get_clauses();
+
+		if ( ! empty( $meta_clauses ) ) {
+			$primary_meta_query = reset( $meta_clauses );
+
+			if ( ! empty( $primary_meta_query['key'] ) ) {
+				$primary_meta_key = $primary_meta_query['key'];
+				$allowed_keys[] = $primary_meta_key;
+			}
+
+			$allowed_keys[] = 'meta_value';
+			$allowed_keys[] = 'meta_value_num';
+			$allowed_keys   = array_merge( $allowed_keys, array_keys( $meta_clauses ) );
+		}
+
+		// If RAND() contains a seed value, sanitize and add to allowed keys.
+		$rand_with_seed = FALSE;
+
+		if ( preg_match( '/RAND\(([0-9]+)\)/i', $orderby, $matches ) ) {
+			$orderby = sprintf( 'RAND(%s)', intval( $matches[1] ) );
+			$allowed_keys[] = $orderby;
+			$rand_with_seed = TRUE;
+		}
+
+		if ( ! in_array( $orderby, $allowed_keys, TRUE ) ) {
+			return FALSE;
+		}
+
+		switch ( $orderby ) {
+			case 'post_name':
+			case 'post_author':
+			case 'post_date':
+			case 'post_title':
+			case 'post_modified':
+			case 'post_parent':
+			case 'post_type':
+			case 'ID':
+			case 'menu_order':
+			case 'comment_count':
+				$orderby_clause = "{$wpdb->posts}.{$orderby}";
+				break;
+
+			case 'rand':
+				$orderby_clause = 'RAND()';
+				break;
+
+			case $primary_meta_key:
+			case 'meta_value':
+				$orderby_clause = ! empty( $primary_meta_query['type'] )
+					? "CAST({$primary_meta_query['alias']}.meta_value AS {$primary_meta_query['cast']})"
+					: "{$primary_meta_query['alias']}.meta_value";
+
+				break;
+
+			case 'meta_value_num':
+				$orderby_clause = "{$primary_meta_query['alias']}.meta_value+0";
+				break;
+
+			default:
+				if ( array_key_exists( $orderby, $meta_clauses ) ) {
+					// $orderby corresponds to a meta_query clause.
+					$meta_clause = $meta_clauses[ $orderby ];
+					$orderby_clause = "CAST({$meta_clause['alias']}.meta_value AS {$meta_clause['cast']})";
+				} elseif ( $rand_with_seed ) {
+					$orderby_clause = $orderby;
+				} else {
+					// Default: order by post field.
+					$orderby_clause = "{$wpdb->posts}.post_" . sanitize_key( $orderby );
+				}
+
+				break;
+		}
+
+		return $orderby_clause;
+	}
+
+	/**
 	 * Parse an 'order' query variable and cast it to ASC or DESC as necessary.
 	 *
 	 * @since 4.0.0
@@ -2099,6 +2194,29 @@ class WP_Query
 			: ( $rand
 				? ''
 				: $this->parse_order( $q['order'] ) );
+
+		// Order by.
+		if ( empty( $q['orderby'] ) ) {
+			// Boolean false or empty array blanks out ORDER BY, while leaving the value unset or otherwise empty sets the default.
+			$orderby = isset( $q['orderby'] )
+			        && ( is_array( $q['orderby'] ) || FALSE === $q['orderby'] )
+				? ''
+				: "{$wpdb->posts}.post_date " . $q['order'];
+		} elseif ( 'none' == $q['orderby'] ) {
+			$orderby = '';
+		} elseif ( $q['orderby'] == 'post__in' && ! empty( $post__in ) ) {
+			$orderby = "FIELD( {$wpdb->posts}.ID, $post__in )";
+		} elseif ( $q['orderby'] == 'post_parent__in' && ! empty( $post_parent__in ) ) {
+			$orderby = "FIELD( {$wpdb->posts}.post_parent, $post_parent__in )";
+		} elseif ( $q['orderby'] == 'post_name__in' && ! empty( $post_name__in ) ) {
+			$orderby = "FIELD( {$wpdb->posts}.post_name, $post_name__in )";
+		} else {
+			$orderby_array = array();
+
+			if ( is_array( $q['orderby'] ) ) {
+				foreach ( $q['orderby'] as $_orderby => $order ) {
+					$orderby = addslashes_gpc( urldecode( $_orderby ) );
+					$parsed = $this->parse_orderby( $orderby );
 /**
  * <- wp-blog-header.php
  * <- wp-load.php
@@ -2110,6 +2228,9 @@ class WP_Query
  * <- wp-includes/post.php
  * @NOW 009: wp-includes/class-wp-query.php
  */
+				}
+			}
+		}
 	}
 
 	/**
