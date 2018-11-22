57bc3b9854b981d87afb4123c41754375847b3f0
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-22 11:54:05 +0900

    Add function: wp_get_attachment_image( int $attachment_id [, string|array $size = 'thumbnail' [, bool $icon = FALSE [, string|array $attr = '']]] )

--- REF/wp-includes/media.php
+++ REF/wp-includes/media.php
@@ -13,8 +13,46 @@
  * <- wp-includes/default-filters.php
  * <- wp-includes/post.php
  * <- wp-includes/post.php
- * @NOW 007: wp-includes/media.php
+ * <- wp-includes/media.php
+ * @NOW 008: wp-includes/media.php
  */
+
+/**
+ * Get an HTML img element representing an image attachment.
+ *
+ * While `$size` will accept an array, it is better to register a size with add_image_size() so that a cropped version is generated.
+ * It's much more efficient than having to find the closest-sized image and then having the browser scale down the image.
+ *
+ * @since 2.5.0
+ *
+ * @param  int          $attachment_id Image attachment ID.
+ * @param  string|array $size          Optional.
+ *                                     Image size.
+ *                                     Accepts any valid image size, or an array of width and height values in pixels (in that order).
+ *                                     Default 'thumbnail'.
+ * @param  bool         $icon          Optional.
+ *                                     Whether the image should be treated as an icon.
+ *                                     Default false.
+ * @param  string|array $attr          Optional.
+ *                                     Attributes for the image markup.
+ *                                     Default empty.
+ * @return string       HTML img element or empty string on failure.
+ */
+function wp_get_attachment_image( $attachment_id, $size = 'thumbnail', $icon = FALSE, $attr = '' )
+{
+	$html = '';
+	$image = wp_get_attachment_image_src( $attachment_id, $size, $icon );
+/**
+ * <- wp-blog-header.php
+ * <- wp-load.php
+ * <- wp-settings.php
+ * <- wp-includes/default-filters.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * @NOW 007: wp-includes/media.php
+ * -> wp-includes/media.php
+ */
+}
 
 /**
  * Returns a filtered list of WP-supported audio formats.
