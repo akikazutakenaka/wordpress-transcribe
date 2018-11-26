4e378a3a9080727f858183c051e98d5d1d8e869a
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-26 14:45:07 +0900

    Finish function: WP_Theme::scandir( string $path [, array|string|null $extensions = NULL [, int $depth = 0 [, string $relative_path = '']]] )

--- REF/wp-includes/class-wp-theme.php
+++ REF/wp-includes/class-wp-theme.php
@@ -448,6 +448,19 @@ final class WP_Theme implements ArrayAccess
 		return wp_cache_get( $key . '-' . $this->cache_hash, 'themes' );
 	}
 
+/**
+ * <- wp-blog-header.php
+ * <- wp-load.php
+ * <- wp-settings.php
+ * <- wp-includes/default-filters.php
+ * <- wp-includes/post.php
+ * <- wp-includes/post.php
+ * <- wp-includes/class-wp-theme.php
+ * <- wp-includes/class-wp-theme.php
+ * <- wp-includes/class-wp-theme.php
+ * @NOW 010: wp-includes/class-wp-theme.php
+ */
+
 	/**
 	 * Return files in the theme's directory.
 	 *
@@ -551,16 +564,73 @@ final class WP_Theme implements ArrayAccess
  */
 	}
 
-/**
- * <- wp-blog-header.php
- * <- wp-load.php
- * <- wp-settings.php
- * <- wp-includes/default-filters.php
- * <- wp-includes/post.php
- * <- wp-includes/post.php
- * <- wp-includes/class-wp-theme.php
- * <- wp-includes/class-wp-theme.php
- * <- wp-includes/class-wp-theme.php
- * @NOW 010: wp-includes/class-wp-theme.php
- */
+	/**
+	 * Scans a directory for files of a certain extension.
+	 *
+	 * @since  3.4.0
+	 * @static
+	 *
+	 * @param  string            $path          Absolute path to search.
+	 * @param  array|string|null $extensions    Optional.
+	 *                                          Array of extensions to find, string of a single extension, or null for all extensions.
+	 *                                          Default null.
+	 * @param  int               $depth         Optional.
+	 *                                          How many levels deep to search for files.
+	 *                                          Accepts 0, 1+, or -1 (infinite depth).
+	 *                                          Default 0.
+	 * @param  string            $relative_path Optional.
+	 *                                          The basename of the absolute path.
+	 *                                          Used to control the returned path for the found files, particularly when this function recurses to lower depths.
+	 *                                          Default empty.
+	 * @return array|false       Array of files, keyed by the path to the file relative to the `$path` directory prepended with `$relative_path`, with the values being absolute paths.
+	 *                           False otherwise.
+	 */
+	private static function scandir( $path, $extensions = NULL, $depth = 0, $relative_path = '' )
+	{
+		if ( ! is_dir( $path ) ) {
+			return FALSE;
+		}
+
+		if ( $extensions ) {
+			$extensions = ( array ) $extensions;
+			$_extensions = array( '|', $extensions );
+		}
+
+		$relative_path = trailingslashit( $relative_path );
+
+		if ( '/' == $relative_path ) {
+			$relative_path = '';
+		}
+
+		$results = scandir( $path );
+		$files = array();
+
+		/**
+		 * Filters the array of excluded directories and files while scanning theme folder.
+		 *
+		 * @since 4.7.4
+		 *
+		 * @param array $exclusions Array of excluded directories and files.
+		 */
+		$exclusions = ( array ) apply_filters( 'theme_scandir_exclusions', array( 'CVS', 'node_modules', 'vendor', 'bower_components' ) );
+
+		foreach ( $results as $result ) {
+			if ( '.' == $result[0] || in_array( $result, $exclusions, TRUE ) ) {
+				continue;
+			}
+
+			if ( is_dir( $path . '/' . $result ) ) {
+				if ( ! $depth ) {
+					continue;
+				}
+
+				$found = self::scandir( $path . '/' . $result, $extensions, $depth - 1, $relative_path . $result );
+				$files = array_merge_recursive( $files, $found );
+			} elseif ( ! $extensions || preg_match( '~\.(' . $_extensions . ')$~', $result ) ) {
+				$files[ $relative_path . $result ] = $path . '/' . $result;
+			}
+		}
+
+		return $files;
+	}
 }
