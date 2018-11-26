6c9caa08df621db008e0b1b1f187d8d5d630adf5
竹中彰一 <akikazu.takenaka@gmail.com>
2018-11-27 02:34:26 +0900

    Add function: WP_Http::request( string $url [, string|array $args = array()] )

--- REF/wp-includes/class-http.php
+++ REF/wp-includes/class-http.php
@@ -89,6 +89,185 @@ class WP_Http
 	const NOT_EXTENDED                    = 510;
 	const NETWORK_AUTHENTICATION_REQUIRED = 511;
 
+	/**
+	 * Send an HTTP request to a URI.
+	 *
+	 * Please note: The only URI that are supported in the HTTP Transport implementation are the HTTP and HTTPS protocols.
+	 *
+	 * @since 2.7.0
+	 *
+	 * @param  string         $url  The request URL.
+	 * @param  string|array   $args {
+	 *     Optional.
+	 *     Array or string of HTTP request arguments.
+	 *
+	 *     @type string       $method              Request method.
+	 *                                             Accepts 'GET', 'POST', 'HEAD', or 'PUT'.
+	 *                                             Some transports technically allow others, but should not be assumed.
+	 *                                             Default 'GET'.
+	 *     @type int          $timeout             How long the connection should stay open in seconds.
+	 *                                             Default 5.
+	 *     @type int          $redirection         Number of allowed redirects.
+	 *                                             Not supported by all transports.
+	 *                                             Default 5.
+	 *     @type string       $httpversion         Version of the HTTP protocol to use.
+	 *                                             Accepts '1.0' and '1.1'.
+	 *                                             Default '1.0'.
+	 *     @type string       $user-agent          User-agent value sent.
+	 *                                             Default 'WordPress/' . get_bloginfo( 'version' ) . '; ' . get_bloginfo( 'url' ).
+	 *     @type bool         $reject_unsafe_urls  Whether to pass URLs through wp_http_validate_url().
+	 *                                             Default false.
+	 *     @type bool         $blocking            Whether the calling code requires the result of the request.
+	 *                                             If set to false, the request will be sent to the remote server, and processing returned to the calling code immediately, the caller will know if the request succeeded or failed, but will not receive any response from the remote server.
+	 *                                             Default true.
+	 *     @type string|array $headers             Array or string of headers to send with the request.
+	 *                                             Default empty array.
+	 *     @type array        $cookies             List of cookies to send with the request.
+	 *                                             Default empty array.
+	 *     @type string|array $body                Body to send with the request.
+	 *                                             Default null.
+	 *     @type bool         $compress            Whether to compress the $body when sending the request.
+	 *                                             Default false.
+	 *     @type bool         $decompress          Whether to decompress a compressed response.
+	 *                                             If set to false and compressed content is returned in the response anyway, it will need to be separately decompressed.
+	 *                                             Default true.
+	 *     @type bool         $sslverify           Whether to verify SSL for the request.
+	 *                                             Default true.
+	 *     @type string       $sslcertificates     Absolute path to an SSL certificate .crt file.
+	 *                                             Default ABSPATH . WPINC . '/certificates/ca-bundle.crt'.
+	 *     @type bool         $stream              Whether to stream to a file.
+	 *                                             If set to true and no filename was given, it will be droped it in the WP temp dir and its name will be set using the basename of the URL.
+	 *                                             Default false.
+	 *     @type string       $filename            Filename of the file to write to when streaming.
+	 *                                             $stream must be set to true.
+	 *                                             Default null.
+	 *     @type int          $limit_response_size Size in bytes to limit the response to.
+	 *                                             Default null.
+	 * }
+	 * @return array|WP_Error Array containing 'headers', 'body', 'response', 'cookies', 'filename'.
+	 *                        A WP_Error instance upon error.
+	 */
+	public function request( $url, $args = array() )
+	{
+		$defaults = array(
+			'method'              => 'GET',
+
+			/**
+			 * Filters the timeout value for an HTTP request.
+			 *
+			 * @since 2.7.0
+			 *
+			 * @param int $timeout_value Time in seconds until a request times out.
+			 *                           Default 5.
+			 */
+			'timeout'             => apply_filters( 'http_request_timeout', 5 ),
+
+			/**
+			 * Filters the number of redirects allowed during an HTTP request.
+			 *
+			 * @since 2.7.0
+			 *
+			 * @param int $redirect_count Number of redirects allowed.
+			 *                            Default 5.
+			 */
+			'redirection'         => apply_filters( 'http_request_redirection_count', 5 ),
+
+			/**
+			 * Filters the version of the HTTP protocol used in a request.
+			 *
+			 * @since 2.7.0
+			 *
+			 * @param string $version Version of HTTP used.
+			 *                        Accepts '1.0' and '1.1'.
+			 *                        Default '1.0'.
+			 */
+			'httpversion'         => apply_filters( 'http_request_version', '1.0' ),
+
+			/**
+			 * Filters the user agent value sent with an HTTP request.
+			 *
+			 * @since 2.7.0
+			 *
+			 * @param string $user_agent WordPress user agent string.
+			 */
+			'user-agent'          => apply_filters( 'http_headers_useragent', 'WordPress/' . get_bloginfo( 'version' ) . '; ' . get_bloginfo( 'url' ) ),
+
+			/**
+			 * Filters whether to pass URLs through wp_http_validate_url() in an HTTP request.
+			 *
+			 * @since 3.6.0
+			 *
+			 * @param bool $pass_url Whether to pass URLs through wp_http_validate_url().
+			 *                       Default false.
+			 */
+			'reject_unsafe_urls'  => apply_filters( 'http_request_reject_unsafe_urls', FALSE ),
+
+			'blocking'            => TRUE,
+			'headers'             => array(),
+			'cookies'             => array(),
+			'body'                => NULL,
+			'compress'            => FALSE,
+			'decompress'          => TRUE,
+			'sslverify'           => TRUE,
+			'sslcertificates'     => ABSPATH . WPINC . '/certificates/ca-bundle.crt',
+			'stream'              => FALSE,
+			'filename'            => NULL,
+			'limit_response_size' => NULL
+		);
+
+		// Pre-parse for the HEAD checks.
+		$args = wp_parse_args( $args );
+
+		// By default, Head requests do not cause redirections.
+		if ( isset( $args['method'] ) && 'HEAD' == $args['method'] ) {
+			$defaults['redirection'] = 0;
+		}
+
+		$r = wp_parse_args( $args, $defaults );
+
+		/**
+		 * Filters the arguments used in an HTTP request.
+		 *
+		 * @since 2.7.0
+		 *
+		 * @param array  $r   An array of HTTP request arguments.
+		 * @param string $url The request URL.
+		 */
+		$r = apply_filters( 'http_request_args', $r, $url );
+
+		// The transports decrement this, store a copy of the original value for loop purposes.
+		if ( ! isset( $r['_redirection'] ) ) {
+			$r['_redirection'] = $r['redirection'];
+		}
+
+		/**
+		 * Filters whether to preempt an HTTP request's return value.
+		 *
+		 * Returning a non-false value from the filter will short-circuit the HTTP request and return early with that value.
+		 * A filter should return either:
+		 *
+		 * - An array containing 'headers', 'body', 'response', 'cookies', and 'filename' elements
+		 * - A WP_Error instance
+		 * - boolean false (to avoid short-circuiting the response)
+		 *
+		 * Returning any other value may result in unexpected behaviour.
+		 *
+		 * @since 2.9.0
+		 *
+		 * @param false|array|WP_Error $preempt Whether to preempt an HTTP request's return value.
+		 *                                      Default false.
+		 * @param array                $r       HTTP request arguments.
+		 * @param string               $url     The request URL.
+		 */
+		$pre = apply_filters( 'pre_http_request', FALSE, $r, $url );
+
+		if ( FALSE !== $pre ) {
+			return $pre;
+		}
+
+		if ( function_exists( 'wp_kses_bad_protocol' ) ) {
+			if ( $r['reject_unsafe_urls'] ) {
+				$url = wp_http_validate_url( $url );
 /**
  * <-......: wp-blog-header.php
  * <-......: wp-load.php
@@ -102,7 +281,11 @@ class WP_Http
  * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
  * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
  * @NOW 012: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
+ * ......->: wp-includes/http.php: wp_http_validate_url( string $url )
  */
+			}
+		}
+	}
 
 	/**
 	 * Tests which transports are capable of supporting the request.
--- REF/wp-includes/http.php
+++ REF/wp-includes/http.php
@@ -81,6 +81,22 @@ function wp_http_supports( $capabilities = array(), $url = NULL )
 }
 
 /**
+ * <-......: wp-blog-header.php
+ * <-......: wp-load.php
+ * <-......: wp-settings.php
+ * <-......: wp-includes/default-filters.php
+ * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
+ * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
+ * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
+ * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
+ * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
+ * @NOW 013: wp-includes/http.php: wp_http_validate_url( string $url )
+ */
+
+/**
  * A wrapper for PHP's parse_url() function that handles consistency in the return values across PHP versions.
  *
  * PHP 5.4.7 expanded parse_url()'s ability to handle non-absolute url's, including schemeless and relative url's with :// in the path.
