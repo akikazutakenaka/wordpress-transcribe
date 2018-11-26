6e5434e8e82b39a9bf78f4c3e0a2b4779f449944
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-26 09:32:52 +0900

    Finish function: wp_calculate_image_srcset( array $size_array, string $image_src, array $image_meta [, int $attachment_id = 0] )

--- REF/wp-includes/media.php
+++ REF/wp-includes/media.php
@@ -572,6 +572,27 @@ function wp_get_attachment_image( $attachment_id, $size = 'thumbnail', $icon = F
 	if ( $image ) {
 		list( $src, $width, $height ) = $image;
 		$hwstring = image_hwstring( $width, $height );
+		$size_class = $size;
+
+		if ( is_array( $size_class ) ) {
+			$size_class = join( 'x', $size_class );
+		}
+
+		$attachment = get_post( $attachment_id );
+		$default_attr = array(
+			'src'   => $src,
+			'class' => "attachment-$size_class size-$size_class",
+			'alt'   => trim( strip_tags( get_post_meta( $attachment_id, '_wp_attachment_image_alt', TRUE ) ) )
+		);
+		$attr = wp_parse_args( $attr, $default_attr );
+
+		// Generate 'srcset' and 'sizes' if not already present.
+		if ( empty( $attr['srcset'] ) ) {
+			$image_meta = wp_get_attachment_metadata( $attachment_id );
+
+			if ( is_array( $image_meta ) ) {
+				$size_array = array( absint( $width ), absint( $height ) );
+				$srcset = wp_calculate_image_srcset( $size_array, $src, $image_meta, $attachment_id );
 /**
  * <- wp-blog-header.php
  * <- wp-load.php
@@ -581,6 +602,8 @@ function wp_get_attachment_image( $attachment_id, $size = 'thumbnail', $icon = F
  * <- wp-includes/post.php
  * @NOW 007: wp-includes/media.php
  */
+			}
+		}
 	}
 }
 
@@ -608,6 +631,191 @@ function _wp_get_attachment_relative_path( $file )
 	}
 
 	return $dirname;
+}
+
+/**
+ * A helper function to calculate the image sources to include in a 'srcset' attribute.
+ *
+ * @since 4.4.0
+ *
+ * @param  array       $size_array    Array of width and height values in pixels (in that order).
+ * @param  string      $image_src     The 'src' of the image.
+ * @param  array       $image_meta    The image meta data as returned by 'wp_get_attachment_metadata()'.
+ * @param  int         $attachment_id Optional.
+ *                                    The image attachment ID to pass to the filter.
+ *                                    Default 0.
+ * @return string|bool The 'srcset' attribute value.
+ *                     False on error or when only one source exists.
+ */
+function wp_calculate_image_srcset( $size_array, $image_src, $image_meta, $attachment_id = 0 )
+{
+	/**
+	 * Let plugins pre-filter the image meta to be able to fix inconsistencies in the stored data.
+	 *
+	 * @since 4.5.0
+	 *
+	 * @param array  $image_meta    The image meta data as returned by 'wp_get_attachment_metadata()'.
+	 * @param array  $size_array    Array of width and height values in pixels (in that order).
+	 * @param string $image_src     The 'src' of the image.
+	 * @param int    $attachment_id The image attachment ID or 0 if not supplied.
+	 */
+	$image_meta = apply_filters( 'wp_calculate_image_srcset_meta', $image_meta, $size_array, $image_src, $attachment_id );
+
+	if ( empty( $image_meta['sizes'] ) || ! isset( $image_meta['file'] ) || strlen( $image_meta['file'] ) < 4 ) {
+		return FALSE;
+	}
+
+	$image_sizes = $image_meta['sizes'];
+
+	// Get the width and height of the image.
+	$image_width = ( int ) $size_array[0];
+	$image_height = ( int ) $size_array[1];
+
+	// Bail early if error/no width.
+	if ( $image_width < 1 ) {
+		return FALSE;
+	}
+
+	$image_basename = wp_basename( $image_meta['file'] );
+
+	/**
+	 * WordPress flattens animated GIFs into one frame when generating intermediate sizes.
+	 * To avoid hiding animation in user content, if src is a full size GIF, a srcset attribute is not generated.
+	 * If src is an intermediate size GIF, the full size is excluded from srcset to keep a flattened GIF from becoming animated.
+	 */
+	if ( ! isset( $image_sizes['thumbnail']['mime-type'] ) || 'image/gif' !== $image_sizes['thumbnail']['mime-type'] ) {
+		$image_sizes[] = array(
+			'width'  => $image_meta['width'],
+			'height' => $image_meta['height'],
+			'file'   => $image_basename
+		);
+	} elseif ( strpos( $image_src, $image_meta['file'] ) ) {
+		return FALSE;
+	}
+
+	// Retrieve the uploads sub-directory from the full size image.
+	$dirname = _wp_get_attachment_relative_path( $image_meta['file'] );
+
+	if ( $dirname ) {
+		$dirname = trailingslashit( $dirname );
+	}
+
+	$upload_dir = wp_get_upload_dir();
+	$image_baseurl = trailingslashit( $upload_dir['baseurl'] )  . $dirname;
+
+	// If currently on HTTPS, prefer HTTPS URLs when we know they're supported by the domain (which is to say, when they share the domain name of the current request).
+	if ( is_ssl() && 'https' !== substr( $image_baseurl, 0, 5 ) && parse_url( $image_baseurl, PHP_URL_HOST ) === $_SERVER['HTTP_HOST'] ) {
+		$image_baseurl = set_url_scheme( $image_baseurl, 'https' );
+	}
+
+	/**
+	 * Images that have been edited in WordPress after being uploaded will contain a unique hash.
+	 * Look for that has and use it later to filter out images that are leftovers from previous versions.
+	 */
+	$image_edited = preg_match( '/-e[0-9]{13}/', wp_basename( $image_src ), $image_edit_hash );
+
+	/**
+	 * Filters the maximum image width to be included in a 'srcset' attribute.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @param int   $max_width  The maximum image width to be included in the 'srcset'.
+	 *                          Default '1600'.
+	 * @param array $size_array Array of width and height values in pixels (in that order).
+	 */
+	$max_srcset_image_width = apply_filters( 'max_srcset_image_width', 1600, $size_array );
+
+	// Array to hold URL candidates.
+	$sources = array();
+
+	/**
+	 * To make sure the ID matches our image src, we will check to see if any sizes in our attachment meta match our $image_src.
+	 * If no matches are found we don't return a srcset to avoid serving an incorrect image.
+	 * See #35045.
+	 */
+	$src_matched = FALSE;
+
+	/**
+	 * Loop through available images.
+	 * Only use images that are resized versions of the same edit.
+	 */
+	foreach ( $image_sizes as $image ) {
+		$is_src = FALSE;
+
+		// Check if image meta isn't corrupted.
+		if ( ! is_array( $image ) ) {
+			continue;
+		}
+
+		// If the file name is part of the `src`, we've confirmed a match.
+		if ( ! $src_matched && FALSE !== strpos( $image_src, $dirname . $image['file'] ) ) {
+			$src_matched = $is_src = TRUE;
+		}
+
+		// Filter out images that are from previous edits.
+		if ( $image_edited && ! strpos( $image['file'], $image_edit_hash[0] ) ) {
+			continue;
+		}
+
+		// Filters out images that are wider than '$max_srcset_image_width' unless that file is in the 'src' attribute.
+		if ( $max_srcset_image_width && $image['width'] > $max_srcset_image_width && ! $is_src ) {
+			continue;
+		}
+
+		// If the image dimensions are within 1px of the expected size, use it.
+		if ( wp_image_matches_ratio( $image_width, $image_height, $image['width'], $image['height'] ) ) {
+			// Add the URL, descriptor, and value to the sources array to be returned.
+			$source = array(
+				'url'        => $image_baseurl . $image['file'],
+				'descriptor' => 'w',
+				'value'      => $image['width']
+			);
+
+			/**
+			 * The 'src' image has to be the first in the 'srcset', because of a bug in iOS8.
+			 * See #35030.
+			 */
+			if ( $is_src ) {
+				$sources = array( $image['width'] => $source ) + $sources;
+			} else {
+				$sources[ $image['width'] ] = $source;
+			}
+		}
+	}
+
+	/**
+	 * Filters an image's 'srcset' sources.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @param array  $sources {
+	 *     One or more arrays of source data to include in the 'srcset'.
+	 *
+	 *     @type array $width {
+	 *         @type string $url        The URL of an image source.
+	 *         @type string $descriptor The descriptor type used in the image candidate string, either 'w' or 'x'.
+	 *         @type int    $value      The source width if paired with a 'w' descriptor, or a pixel density value if paired with an 'x' descriptor.
+	 *     }
+	 * }
+	 * @param array  $size_array    Array of width and height values in pixels (in that order).
+	 * @param string $image_src     The 'src' of the image.
+	 * @param array  $image_meta    The image meta data as returned by 'wp_get_attachment_metadata()'.
+	 * @param int    $attachment_id Image attachment ID or 0.
+	 */
+	$sources = apply_filters( 'wp_calculate_image_srcset', $sources, $size_array, $image_src, $image_meta, $attachment_id );
+
+	// Only return a 'srcset' value if there is more than one source.
+	if ( ! $src_matched || ! is_array( $sources ) || count( $sources ) < 2 ) {
+		return FALSE;
+	}
+
+	$srcset = '';
+
+	foreach ( $sources as $source ) {
+		$srcset .= str_replace( ' ', '%20', $source['url'] ) . ' ' . $source['value'] . $source['descriptor'] . ', ';
+	}
+
+	return rtrim( $srcset, ', ' );
 }
 
 /**
