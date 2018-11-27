f1240700b3dc3ec146adaf1cb9cd2cfa856050fe
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-27 14:02:41 +0900

    Add function: Requests_Cookie::normalize()

--- REF/wp-includes/Requests/Cookie.php
+++ REF/wp-includes/Requests/Cookie.php
@@ -85,6 +85,16 @@ class Requests_Cookie
 		$this->normalize();
 	}
 
+	/**
+	 * Normalize cookie and attributes.
+	 *
+	 * @return bool Whether the cookie was successfully normalized.
+	 */
+	public function normalize()
+	{
+		foreach ( $this->attributes as $key => $value ) {
+			$orig_value = $value;
+			$value = $this->normalize_attribute( $key, $value );
 /**
  * <-......: wp-blog-header.php
  * <-......: wp-load.php
@@ -100,5 +110,26 @@ class Requests_Cookie
  * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
  * <-......: wp-includes/class-http.php: WP_Http::normalize_cookies( array $cookies )
  * @NOW 014: wp-includes/Requests/Cookie.php: Requests_Cookie::normalize()
+ * ......->: wp-includes/Requests/Cookie.php: Requests_Cookie::normalize_attribute( string $name, string|bool $avlue )
+ */
+		}
+	}
+
+/**
+ * <-......: wp-blog-header.php
+ * <-......: wp-load.php
+ * <-......: wp-settings.php
+ * <-......: wp-includes/default-filters.php
+ * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
+ * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
+ * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
+ * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
+ * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
+ * <-......: wp-includes/class-http.php: WP_Http::normalize_cookies( array $cookies )
+ * <-......: wp-includes/Requests/Cookie.php: Requests_Cookie::normalize()
+ * @NOW 015: wp-includes/Requests/Cookie.php: Requests_Cookie::normalize_attribute( string $name, string|bool $avlue )
  */
 }
