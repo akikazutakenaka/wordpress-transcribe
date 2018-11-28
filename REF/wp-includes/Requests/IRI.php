46443d47118f4be9c306cc2ea6f924fecb66243a
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-28 15:40:26 +0900

    Add function: Requests_IRI::set_iri( string $iri )

--- REF/wp-includes/Requests/IRI.php
+++ REF/wp-includes/Requests/IRI.php
@@ -122,6 +122,54 @@ class Requests_IRI
  * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
  * <-......: wp-includes/Requests/Cookie/Jar.php: Requests_Cookie_Jar::register( Requests_Hooker $hooks )
  * <-......: wp-includes/Requests/Cookie/Jar.php: Requests_Cookie_Jar::before_request( string $url, &array $headers, &array $data, &string $type, &array $options )
- * @NOW 017: wp-includes/Requests/IRI.php: Requests_IRI::set_iri( string $iri )
+ * <-......: wp-includes/Requests/IRI.php: Requests_IRI::set_iri( string $iri )
+ * @NOW 018: wp-includes/Requests/IRI.php: Requests_IRI::parse_iri( string $iri )
  */
+
+	/**
+	 * Set the entire IRI.
+	 * Returns true on success, false on failure (if there are any invalid characters).
+	 *
+	 * @param  string $iri
+	 * @return bool
+	 */
+	protected function set_iri( $iri )
+	{
+		static $cache;
+
+		if ( ! $cache ) {
+			$cache = array();
+		}
+
+		if ( $iri === NULL ) {
+			return TRUE;
+		}
+
+		if ( isset( $cache[ $iri ] ) ) {
+			list( $this->scheme, $this->iuserinfo, $this->ihost, $this->port, $this->ipath, $this->iquery, $this->ifragment, $return ) = $cache[ $iri ];
+			return $return;
+		}
+
+		$parsed = $this->parse_iri( ( string ) $iri );
+/**
+ * <-......: wp-blog-header.php
+ * <-......: wp-load.php
+ * <-......: wp-settings.php
+ * <-......: wp-includes/default-filters.php
+ * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
+ * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
+ * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
+ * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
+ * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
+ * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
+ * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
+ * <-......: wp-includes/Requests/Cookie/Jar.php: Requests_Cookie_Jar::register( Requests_Hooker $hooks )
+ * <-......: wp-includes/Requests/Cookie/Jar.php: Requests_Cookie_Jar::before_request( string $url, &array $headers, &array $data, &string $type, &array $options )
+ * @NOW 017: wp-includes/Requests/IRI.php: Requests_IRI::set_iri( string $iri )
+ * ......->: wp-includes/Requests/IRI.php: Requests_IRI::parse_iri( string $iri )
+ */
+	}
 }
