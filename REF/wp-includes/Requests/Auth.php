d635945a2b67ffa06c7acdb6002508243194d443
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-28 11:56:53 +0900

    Add interface: Requests_Auth

--- REF/wp-includes/Requests/Auth.php
+++ REF/wp-includes/Requests/Auth.php
@@ -7,20 +7,15 @@
  */
 
 /**
- * <-......: wp-blog-header.php
- * <-......: wp-load.php
- * <-......: wp-settings.php
- * <-......: wp-includes/default-filters.php
- * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
- * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
- * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
- * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
- * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
- * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
- * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
- * <-......: wp-includes/Requests/Auth/Basic: Requests_Auth_Basic
- * @NOW 016: wp-includes/Requests/Auth.php: Requests_Auth
+ * Authentication provider interface.
+ *
+ * Implement this interface to act as an authentication provider.
+ *
+ * Parameters should be passed via the constructor where possible, as this makes it much easier for users to use your provider.
+ *
+ * @see        Requests_Hooks
+ * @package    Requests
+ * @subpackage Authentication
  */
+interface Requests_Auth
+{}
--- REF/wp-includes/Requests/Auth/Basic.php
+++ REF/wp-includes/Requests/Auth/Basic.php
@@ -45,7 +45,6 @@ class Requests_Auth_Basic implements Requests_Auth
  * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
  * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
  * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
- * @NOW 015: wp-includes/Requests/Auth/Basic: Requests_Auth_Basic
- * ......->: wp-includes/Requests/Auth.php: Requests_Auth
+ * @NOW 015: wp-includes/Requests/Auth/Basic: Requests_Auth_Basic::__construct( [array|null $args = NULL] )
  */
 }
