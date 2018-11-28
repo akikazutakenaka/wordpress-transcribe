0b46b7b19c44b615a11df989d6aadbcf559c3feb
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-28 14:59:11 +0900

    Finish function: Requests_Proxy_HTTP::fsockopen_header( &string $out ) Finish class: Requests_Proxy_HTTP Finish file: wp-includes/Requests/Proxy/HTTP.php

--- REF/wp-includes/Requests/Proxy/HTTP.php
+++ REF/wp-includes/Requests/Proxy/HTTP.php
@@ -139,23 +139,17 @@ class Requests_Proxy_HTTP implements Requests_Proxy
 		$path = $url;
 	}
 
-/**
- * <-......: wp-blog-header.php
- * <-......: wp-load.php
- * <-......: wp-settings.php
- * <-......: wp-includes/default-filters.php
- * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
- * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
- * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
- * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
- * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
- * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
- * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
- * @NOW 015: wp-includes/Requests/Proxy/HTTP.php: Requests_Proxy_HTTP::fsockopen_header( &string $out )
- */
+	/**
+	 * Add extra headers to the request before sending.
+	 *
+	 * @since 1.6
+	 *
+	 * @param string $out HTTP header string.
+	 */
+	public function fsockopen_header( &$out )
+	{
+		$out .= sprintf( "Proxy-Authorization: Basic %s\r\n", base64_encode( $this->get_auth_string() ) );
+	}
 
 	/**
 	 * Get the authentication string (user:pass).
