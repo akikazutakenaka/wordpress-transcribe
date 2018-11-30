af118fe2639759685bb967ef31fcf4829825d394
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-30 09:12:30 +0900

    Finish function: Requests_IPv6::uncompress( string $ip )

--- ORI/wp-includes/Requests/IPv6.php
+++ ORI/wp-includes/Requests/IPv6.php
@@ -16,57 +16,7 @@
  * @subpackage Utilities
  */
 class Requests_IPv6 {
-	/**
-	 * Uncompresses an IPv6 address
-	 *
-	 * RFC 4291 allows you to compress consecutive zero pieces in an address to
-	 * '::'. This method expects a valid IPv6 address and expands the '::' to
-	 * the required number of zero pieces.
-	 *
-	 * Example:  FF01::101   ->  FF01:0:0:0:0:0:0:101
-	 *           ::1         ->  0:0:0:0:0:0:0:1
-	 *
-	 * @author Alexander Merz <alexander.merz@web.de>
-	 * @author elfrink at introweb dot nl
-	 * @author Josh Peck <jmp at joshpeck dot org>
-	 * @copyright 2003-2005 The PHP Group
-	 * @license http://www.opensource.org/licenses/bsd-license.php
-	 * @param string $ip An IPv6 address
-	 * @return string The uncompressed IPv6 address
-	 */
-	public static function uncompress($ip) {
-		if (substr_count($ip, '::') !== 1) {
-			return $ip;
-		}
-
-		list($ip1, $ip2) = explode('::', $ip);
-		$c1 = ($ip1 === '') ? -1 : substr_count($ip1, ':');
-		$c2 = ($ip2 === '') ? -1 : substr_count($ip2, ':');
-
-		if (strpos($ip2, '.') !== false) {
-			$c2++;
-		}
-		// ::
-		if ($c1 === -1 && $c2 === -1) {
-			$ip = '0:0:0:0:0:0:0:0';
-		}
-		// ::xxx
-		else if ($c1 === -1) {
-			$fill = str_repeat('0:', 7 - $c2);
-			$ip = str_replace('::', $fill, $ip);
-		}
-		// xxx::
-		else if ($c2 === -1) {
-			$fill = str_repeat(':0', 7 - $c1);
-			$ip = str_replace('::', $fill, $ip);
-		}
-		// xxx::xxx
-		else {
-			$fill = ':' . str_repeat('0:', 6 - $c2 - $c1);
-			$ip = str_replace('::', $fill, $ip);
-		}
-		return $ip;
-	}
+	// refactored. public static function uncompress($ip) {}
 
 	/**
 	 * Compresses an IPv6 address
--- REF/wp-includes/Requests/IPv6.php
+++ REF/wp-includes/Requests/IPv6.php
@@ -16,29 +16,63 @@
  */
 class Requests_IPv6
 {
-/**
- * <-......: wp-blog-header.php
- * <-......: wp-load.php
- * <-......: wp-settings.php
- * <-......: wp-includes/default-filters.php
- * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
- * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
- * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
- * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
- * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
- * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
- * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
- * <-......: wp-includes/Requests/Cookie/Jar.php: Requests_Cookie_Jar::register( Requests_Hooker $hooks )
- * <-......: wp-includes/Requests/Cookie/Jar.php: Requests_Cookie_Jar::before_request( string $url, &array $headers, &array $data, &string $type, &array $options )
- * <-......: wp-includes/Requests/IRI.php: Requests_IRI::set_iri( string $iri )
- * <-......: wp-includes/Requests/IRI.php: Requests_IRI::set_authority( string $authority )
- * <-......: wp-includes/Requests/IRI.php: Requests_IRI::set_host( string $host )
- * <-......: wp-includes/Requests/IPv6.php: Requests_IPv6::check_ipv6( string $ip )
- * @NOW 021: wp-includes/Requests/IPv6.php: Requests_IPv6::uncompress( string $ip )
- */
+	/**
+	 * Uncompresses an IPv6 address.
+	 *
+	 * RFC 4291 allows you to compress consecutive zero pieces in an address to '::'.
+	 * This method expects a valid IPv6 address and expands the '::' to the required number of zero pieces.
+	 *
+	 * Example: FF01::101 -> FF01:0:0:0:0:0:0:101
+	 *          ::1       -> 0:0:0:0:0:0:0:1
+	 *
+	 * @author    Alexander Merz <alexander.merz@web.de>
+	 * @author    elfrink@introweb.nl
+	 * @author    Josh Peck <jmp@joshpeck.org>
+	 * @copyright 2003-2005 The PHP Group
+	 * @license   http://www.opensource.org/licenses/bsd-license.php
+	 *
+	 * @param  string $ip An IPv6 address.
+	 * @return string The uncompressed IPv6 address.
+	 */
+	public static function uncompress( $ip )
+	{
+		if ( substr_count( $ip, '::' ) !== 1 ) {
+			return $ip;
+		}
+
+		list( $ip1, $ip2 ) = explode( '::', $ip );
+
+		$c1 = $ip1 === ''
+			? -1
+			: substr_count( $ip1, ':' );
+
+		$c2 = $ip2 === ''
+			? -1
+			: substr_count( $ip2, ':' );
+
+		if ( strpos( $ip2, '.' ) !== FALSE ) {
+			$c2++;
+		}
+
+		if ( $c1 === -1 && $c2 === -1 ) {
+			// ::
+			$ip = '0:0:0:0:0:0:0:0';
+		} elseif ( $c1 === -1 ) {
+			// ::xxx
+			$fill = str_repeat( '0:', 7 - $c2 );
+			$ip = str_replace( '::', $fill, $ip );
+		} elseif ( $c2 === -1 ) {
+			// xxx::
+			$fill = str_repeat( ':0', 7 - $c1 );
+			$ip = str_replace( '::', $fill, $ip );
+		} else {
+			// xxx::xxx
+			$fill = ':' . str_repeat( '0:', 6 - $c2 - $c1 );
+			$ip = str_replace( '::', $fill, $ip );
+		}
+
+		return $ip;
+	}
 
 	/**
 	 * Checks an IPv6 address.
@@ -72,7 +106,6 @@ class Requests_IPv6
  * <-......: wp-includes/Requests/IRI.php: Requests_IRI::set_authority( string $authority )
  * <-......: wp-includes/Requests/IRI.php: Requests_IRI::set_host( string $host )
  * @NOW 020: wp-includes/Requests/IPv6.php: Requests_IPv6::check_ipv6( string $ip )
- * ......->: wp-includes/Requests/IPv6.php: Requests_IPv6::uncompress( string $ip )
  */
 	}
 }
