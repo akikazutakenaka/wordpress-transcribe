a919d0ca42ab98544025eb9265b3cba8ebabe665
竹中彰一 <akikazu.takenaka@gmail.com>
2018-12-01 01:46:53 +0900

    Finish function: Requests_Transport_cURL::setup_handle( string $url, array $headers, string|array $data, array $options )

--- REF/wp-includes/Requests/Transport/cURL.php
+++ REF/wp-includes/Requests/Transport/cURL.php
@@ -151,7 +151,6 @@ class Requests_Transport_cURL implements Requests_Transport
  * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
  * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
  * @NOW 014: wp-includes/Requests/Transport/cURL.php: Requests_Transport_cURL::request( string $url [, array $headers = array() [, string|array $data = array() [, array $options = array()]]] )
- * ......->: wp-includes/Requests/Transport/cURL.php: Requests_Transport_cURL::setup_handle( string $url, array $headers, string|array $data, array $options )
  */
 	}
 
@@ -249,23 +248,7 @@ class Requests_Transport_cURL implements Requests_Transport
 		if ( TRUE === $options['blocking'] ) {
 			curl_setopt( $this->handle, CURLOPT_HEADERFUNCTION, array( &$this, 'stream_headers' ) );
 			curl_setopt( $this->handle, CURLOPT_WRITEFUNCTION, array( &$this, 'stream_body' ) );
-/**
- * <-......: wp-blog-header.php
- * <-......: wp-load.php
- * <-......: wp-settings.php
- * <-......: wp-includes/default-filters.php
- * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
- * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
- * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
- * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
- * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
- * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
- * <-......: wp-includes/Requests/Transport/cURL.php: Requests_Transport_cURL::request( string $url [, array $headers = array() [, string|array $data = array() [, array $options = array()]]] )
- * @NOW 015: wp-includes/Requests/Transport/cURL.php: Requests_Transport_cURL::setup_handle( string $url, array $headers, string|array $data, array $options )
- */
+			curl_setopt( $this->handle, CURLOPT_BUFFERSIZE, Requests::BUFFER_SIZE );
 		}
 	}
 
