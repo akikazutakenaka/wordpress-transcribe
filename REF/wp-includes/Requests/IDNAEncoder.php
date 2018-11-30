f08dc7b0bf60cffee7574200db1c1394081b683d
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-30 12:35:47 +0900

    Add function: Requests_IDNAEncoder::encode( string $string )

--- REF/wp-includes/Requests/IDNAEncoder.php
+++ REF/wp-includes/Requests/IDNAEncoder.php
@@ -36,6 +36,18 @@ class Requests_IDNAEncoder
 	const BOOTSTRAP_INITIAL_BIAS = 72;
 	const BOOTSTRAP_INITIAL_N    = 128;
 
+	/**
+	 * Encode a hostname using Punycode.
+	 *
+	 * @param  string $string Hostname.
+	 * @return string Punycode-encoded hostname.
+	 */
+	public static function encode( $string )
+	{
+		$parts = explode( '.', $string );
+
+		foreach ( $parts as &$part ) {
+			$part = self::to_ascii( $part );
 /**
  * <-......: wp-blog-header.php
  * <-......: wp-load.php
@@ -51,6 +63,28 @@ class Requests_IDNAEncoder
  * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
  * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
  * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
- * @NOW 015: wp-includes/Requests/IDNAEncoder.php: Requests/IDNAEncoder::encode( string $string )
+ * @NOW 015: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::encode( string $string )
+ * ......->: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::to_ascii( string $string )
+ */
+		}
+	}
+
+/**
+ * <-......: wp-blog-header.php
+ * <-......: wp-load.php
+ * <-......: wp-settings.php
+ * <-......: wp-includes/default-filters.php
+ * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
+ * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
+ * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
+ * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
+ * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
+ * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
+ * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
+ * <-......: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::encode( string $string )
+ * @NOW 016: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::to_ascii( string $string )
  */
 }
