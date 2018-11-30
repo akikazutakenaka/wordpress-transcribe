8d9712767b48b265d6d8412f5bb92d7043b4205f
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-11-30 14:43:07 +0900

    Finish function: Requests_IDNAEncoder::to_ascii( string $string )

--- ORI/wp-includes/Requests/IDNAEncoder.php
+++ ORI/wp-includes/Requests/IDNAEncoder.php
@@ -29,62 +29,7 @@ class Requests_IDNAEncoder {
 		return implode('.', $parts);
 	}
 
-	/**
-	 * Convert a UTF-8 string to an ASCII string using Punycode
-	 *
-	 * @throws Requests_Exception Provided string longer than 64 ASCII characters (`idna.provided_too_long`)
-	 * @throws Requests_Exception Prepared string longer than 64 ASCII characters (`idna.prepared_too_long`)
-	 * @throws Requests_Exception Provided string already begins with xn-- (`idna.provided_is_prefixed`)
-	 * @throws Requests_Exception Encoded string longer than 64 ASCII characters (`idna.encoded_too_long`)
-	 *
-	 * @param string $string ASCII or UTF-8 string (max length 64 characters)
-	 * @return string ASCII string
-	 */
-	public static function to_ascii($string) {
-		// Step 1: Check if the string is already ASCII
-		if (self::is_ascii($string)) {
-			// Skip to step 7
-			if (strlen($string) < 64) {
-				return $string;
-			}
-
-			throw new Requests_Exception('Provided string is too long', 'idna.provided_too_long', $string);
-		}
-
-		// Step 2: nameprep
-		$string = self::nameprep($string);
-
-		// Step 3: UseSTD3ASCIIRules is false, continue
-		// Step 4: Check if it's ASCII now
-		if (self::is_ascii($string)) {
-			// Skip to step 7
-			if (strlen($string) < 64) {
-				return $string;
-			}
-
-			throw new Requests_Exception('Prepared string is too long', 'idna.prepared_too_long', $string);
-		}
-
-		// Step 5: Check ACE prefix
-		if (strpos($string, self::ACE_PREFIX) === 0) {
-			throw new Requests_Exception('Provided string begins with ACE prefix', 'idna.provided_is_prefixed', $string);
-		}
-
-		// Step 6: Encode with Punycode
-		$string = self::punycode_encode($string);
-
-		// Step 7: Prepend ACE prefix
-		$string = self::ACE_PREFIX . $string;
-
-		// Step 8: Check size
-		if (strlen($string) < 64) {
-			return $string;
-		}
-
-		throw new Requests_Exception('Encoded string is too long', 'idna.encoded_too_long', $string);
-	}
-
-	// refactored. protected static function is_ascii($string) {}
+	// refactored. public static function to_ascii($string) {}
 	// :
 	// refactored. protected static function adapt($delta, $numpoints, $firsttime) {}
 }
--- REF/wp-includes/Requests/IDNAEncoder.php
+++ REF/wp-includes/Requests/IDNAEncoder.php
@@ -64,7 +64,6 @@ class Requests_IDNAEncoder
  * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
  * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
  * @NOW 015: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::encode( string $string )
- * ......->: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::to_ascii( string $string )
  */
 		}
 	}
@@ -113,24 +112,16 @@ class Requests_IDNAEncoder
 
 		// Step 6: Encode with Punycode.
 		$string = self::punycode_encode( $string );
-/**
- * <-......: wp-blog-header.php
- * <-......: wp-load.php
- * <-......: wp-settings.php
- * <-......: wp-includes/default-filters.php
- * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
- * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
- * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
- * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
- * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
- * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
- * <-......: wp-includes/class-requests.php: Requests::set_defaults( &string $url, &array $headers, &array|null $data, &string $type, &array $options )
- * <-......: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::encode( string $string )
- * @NOW 016: wp-includes/Requests/IDNAEncoder.php: Requests_IDNAEncoder::to_ascii( string $string )
- */
+
+		// Step 7: Prepend ACE prefix.
+		$string = self::ACE_PREFIX . $string;
+
+		// Step 8: Check size.
+		if ( strlen( $string ) < 64 ) {
+			return $string;
+		}
+
+		throw new Requests_Exception( 'Encoded string is too long', 'idna.encoded_too_long', $string );
 	}
 
 	/**
