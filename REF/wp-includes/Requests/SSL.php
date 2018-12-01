090ec55d6c3910961abd179db7a1c4dcdbe31707
竹中彰一 <akikazu.takenaka@gmail.com>
2018-12-02 01:36:26 +0900

    Finish function: Requests_SSL::verify_certificate( string $host, array $cert ) Finish class: Requests_SSL Finish file: wp-includes/Requests/SSL.php

--- REF/wp-includes/Requests/SSL.php
+++ REF/wp-includes/Requests/SSL.php
@@ -58,27 +58,20 @@ class Requests_SSL
 
 				// Check for a match.
 				if ( self::match_domain( $host, $altname ) === TRUE ) {
-/**
- * <-......: wp-blog-header.php
- * <-......: wp-load.php
- * <-......: wp-settings.php
- * <-......: wp-includes/default-filters.php
- * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
- * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
- * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
- * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
- * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
- * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
- * <-......: wp-includes/Requests/Transport/fsockopen.php: Requests_Transport_fsockopen::request( string $url [, array $headers = array() [, string|array $data = array() [, array $options = array()]]] )
- * <-......: wp-includes/Requests/Transport/fsockopen.php: verify_certificate_from_context( string $host, resource $context )
- * @NOW 016: wp-includes/Requests/SSL.php: Requests_SSL::verity_certificate( string $host, array $cert )
- */
+					return TRUE;
 				}
 			}
 		}
+
+		// Fall back to checking the common name if we didn't get any DNS name alt names, as per RFC 2818.
+		if ( ! $has_dns_alt && ! empty( $cert['subject']['CN'] ) ) {
+			// Check for a match.
+			if ( self::match_domain( $host, $cert['subject']['CN'] ) === TRUE ) {
+				return TRUE;
+			}
+		}
+
+		return FALSE;
 	}
 
 	/**
