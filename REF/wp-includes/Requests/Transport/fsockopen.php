d8b183ca1d210f54eb864be811620758fa286a86
竹中彰一 <akikazu.takenaka@gmail.com>
2018-12-02 01:20:34 +0900

    Add function: Requests_Transport_fsockopen::verify_certificate_from_context( string $host, resource $context ) Add file: wp-includes/Requests/SSL.php

--- REF/wp-includes/Requests/Transport/fsockopen.php
+++ REF/wp-includes/Requests/Transport/fsockopen.php
@@ -119,6 +119,11 @@ class Requests_Transport_fsockopen implements Requests_Transport
 
 		$remote_socket .= ':' . $url_parts['port'];
 		set_error_handler( array( $this, 'connect_error_handler' ), E_WARNING | E_NOTICE );
+		$options['hooks']->dispatch( 'fsockopen.remote_socket', array( &$remote_socket ) );
+		$socket = stream_socket_client( $remote_socket, $errno, $errstr, ceil( $options['connect_timeout'] ), STREAM_CLIENT_CONNECT, $context );
+		restore_error_handler();
+
+		if ( $verifyname && ! $this->verify_certificate_from_context( $host, $context ) ) {
 /**
  * <-......: wp-blog-header.php
  * <-......: wp-load.php
@@ -134,7 +139,9 @@ class Requests_Transport_fsockopen implements Requests_Transport
  * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
  * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
  * @NOW 014: wp-includes/Requests/Transport/fsockopen.php: Requests_Transport_fsockopen::request( string $url [, array $headers = array() [, string|array $data = array() [, array $options = array()]]] )
+ * ......->: wp-includes/Requests/Transport/fsockopen.php: verify_certificate_from_context( string $host, resource $context )
  */
+		}
 	}
 
 	/**
@@ -154,6 +161,50 @@ class Requests_Transport_fsockopen implements Requests_Transport
 		$this->connect_error .= $errstr . "\n";
 		return TRUE;
 	}
+
+	/**
+	 * Verify the certificate against common name and subject alternative names.
+	 *
+	 * Unfortunately, PHP doesn't check the certificate against the alternative names, leading things like 'https://www.github.com/' to be invalid.
+	 *
+	 * @see    https://tools.ietf.org/html/rfc2818#section-3.1 RFC2818, Section 3.1
+	 * @throws Requests_Exception On failure to connect via TLS (`fsockopen.ssl.connect_error`)
+	 * @throws Requests_Exception On not obtaining a match for the host (`fsockopen.ssl.no_match`)
+	 *
+	 * @param  string   $host    Host name to verify against.
+	 * @param  resource $context Stream context.
+	 * @return bool
+	 */
+	public function verify_certificate_from_context( $host, $context )
+	{
+		$meta = stream_context_get_options( $context );
+
+		// If we don't have SSL options, then we couldn't make the connection at all.
+		if ( empty( $meta ) || empty( $meta['ssl'] ) || empty( $meta['ssl']['peer_certificate'] ) ) {
+			throw new Requests_Exception( rtrim( $this->connect_error ), 'ssl.connect_error' );
+		}
+
+		$cert = openssl_x509_parse( $meta['ssl']['peer_certificate'] );
+		return Requests_SSL::verify_certificate( $host, $cert );
+	}
+/**
+ * <-......: wp-blog-header.php
+ * <-......: wp-load.php
+ * <-......: wp-settings.php
+ * <-......: wp-includes/default-filters.php
+ * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
+ * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
+ * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
+ * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
+ * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
+ * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
+ * <-......: wp-includes/class-requests.php: Requests::request( string $url [, array $headers = array() [, array|null $data = array() [, string $type = self::GET [, array $options = array()]]]] )
+ * <-......: wp-includes/Requests/Transport/fsockopen.php: Requests_Transport_fsockopen::request( string $url [, array $headers = array() [, string|array $data = array() [, array $options = array()]]] )
+ * @NOW 015: wp-includes/Requests/Transport/fsockopen.php: verify_certificate_from_context( string $host, resource $context )
+ * ......->: wp-includes/Requests/SSL.php: Requests_SSL
+ */
 
 	/**
 	 * Whether this transport is valid.
