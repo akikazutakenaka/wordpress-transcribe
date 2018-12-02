042c16d99ab186d4597eb0766b4dd33e45b27a91
竹中彰一 <akikazu.takenaka@gmail.com>
2018-12-03 02:50:31 +0900

    Finish function: Requests::parse_response( string $headers, string $url, array $req_headers, array $req_data, array $options )

--- REF/wp-includes/class-requests.php
+++ REF/wp-includes/class-requests.php
@@ -476,24 +476,22 @@ class Requests
 				if ( strpos( $location, 'http://' ) !== 0 && strpos( $location, 'https://' ) !== 0 ) {
 					// Relative redirect, for compatibility make it absolute.
 					$location = Requests_IRI::absolutize( $url, $location );
-/**
- * <-......: wp-blog-header.php
- * <-......: wp-load.php
- * <-......: wp-settings.php
- * <-......: wp-includes/default-filters.php
- * <-......: wp-includes/post.php: wp_check_post_hierarchy_for_loops( int $post_parent, int $post_ID )
- * <-......: wp-includes/post.php: wp_insert_post( array $postarr [, bool $wp_error = FALSE] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_page_templates( [WP_Post|null $post = NULL [, string $post_type = 'page']] )
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::get_post_templates()
- * <-......: wp-includes/class-wp-theme.php: WP_Theme::translate_header( string $header, string $value )
- * <-......: wp-admin/includes/theme.php: get_theme_feature_list( [bool $api = TRUE] )
- * <-......: wp-admin/includes/theme.php: themes_api( string $action [, array|object $args = array()] )
- * <-......: wp-includes/class-http.php: WP_Http::request( string $url [, string|array $args = array()] )
- * @NOW 013: wp-includes/class-requests.php: Requests::parse_response( string $headers, string $url, array $req_headers, array $req_data, array $options )
- */
+					$location = $location->uri;
 				}
+
+				$hook_args = array( &$location, &$req_headers, &$req_data, &$options, $return );
+				$options['hooks']->dispatch( 'requests.before_redirect', $hook_args );
+				$redirected = self::request( $location, $req_headers, $req_data, $options['type'], $options );
+				$redirected->history[] = $return;
+				return $redirected;
+			} elseif ( $options['redirected'] >= $options['redirected'] ) {
+				throw new Requests_Exception( 'Too many redirects', 'toomanyredirects', $return );
 			}
 		}
+
+		$return->redirects = $options['redirected'];
+		$options['hooks']->dispatch( 'requests.after_request', array( &$return, $req_headers, $req_data, $options ) );
+		return $return;
 	}
 
 	/**
