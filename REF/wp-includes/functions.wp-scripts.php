62ddc2f4d76b0406234fb6082920a9b3996a5370
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-12-04 16:19:23 +0900

    Add function: wp_enqueue_script( string $handle [, string $src = '' [, array $deps = array() [, string|bool|null $ver = FALSE [, bool $in_footer = FALSE]]]] )

--- REF/wp-includes/functions.wp-scripts.php
+++ REF/wp-includes/functions.wp-scripts.php
@@ -14,5 +14,46 @@
  * <-......: wp-includes/default-filters.php
  * <-......: wp-includes/post-template.php: prepend_attachment( string $content )
  * <-......: wp-includes/media.php: wp_video_shortcode( array $attr [, string $content = ''] )
- * @NOW 007: wp-includes/functions.wp-scripts.php: wp_enqueue_script( string $handle [, string $src = '' [, array $deps = array() [, string|bool|null $ver = FALSE [, bool $in_footer = FALSE]]]] )
+ * <-......: wp-includes/functions.wp-scripts.php: wp_enqueue_script( string $handle [, string $src = '' [, array $deps = array() [, string|bool|null $ver = FALSE [, bool $in_footer = FALSE]]]] )
+ * @NOW 008: wp-includes/functions.wp-scripts.php: wp_scripts()
  */
+
+/**
+ * Enqueue a script.
+ *
+ * Registers the script if $src provided (does NOT overwrite), and enqueues it.
+ *
+ * @see   WP_Dependencies::add()
+ * @see   WP_Dependencies::add_data()
+ * @see   WP_Dependencies::enqueue()
+ * @since 2.1.0
+ *
+ * @param string           $handle    Name of the script.
+ *                                    Should be unique.
+ * @param string           $src       Full URL of the script, or path of the script relative to the WordPress root directory.
+ *                                    Default empty.
+ * @param array            $deps      Optional.
+ *                                    An array of registered script handles this script depends on.
+ *                                    Default empty array.
+ * @param string|bool|null $ver       Optional.
+ *                                    String specifying script version number, if it has one, which is added to the URL as a query string for cache busting purposes.
+ *                                    If version is set to false, a version number is automatically added equal to current installed WordPress version.
+ *                                    If set to null, no version is added.
+ * @param bool             $in_footer Optional.
+ *                                    Whether to enqueue the script before </body> instead of in the <head>.
+ *                                    Default 'false'.
+ */
+function wp_enqueue_script( $handle, $src = '', $deps = array(), $ver = FALSE, $in_footer = FALSE )
+{
+	$wp_scripts = wp_scripts();
+/**
+ * <-......: wp-blog-header.php
+ * <-......: wp-load.php
+ * <-......: wp-settings.php
+ * <-......: wp-includes/default-filters.php
+ * <-......: wp-includes/post-template.php: prepend_attachment( string $content )
+ * <-......: wp-includes/media.php: wp_video_shortcode( array $attr [, string $content = ''] )
+ * @NOW 007: wp-includes/functions.wp-scripts.php: wp_enqueue_script( string $handle [, string $src = '' [, array $deps = array() [, string|bool|null $ver = FALSE [, bool $in_footer = FALSE]]]] )
+ * ......->: wp-includes/functions.wp-scripts.php: wp_scripts()
+ */
+}
