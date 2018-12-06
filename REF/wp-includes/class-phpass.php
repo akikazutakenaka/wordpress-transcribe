d9d703b1f897c98d4db79ad25541bc799bf66309
竹中彰一 <akikazu-takenaka@hitowa.com>
2018-12-06 13:03:07 +0900

    Add function: PasswordHash::crypt_private( string $password, string $setting )

--- REF/wp-includes/class-phpass.php
+++ REF/wp-includes/class-phpass.php
@@ -77,8 +77,76 @@ class PasswordHash
  * <-......: wp-includes/post-template.php: get_the_content( [string $more_link_text = NULL [, bool $strip_teaser = FALSE]] )
  * <-......: wp-includes/post-template.php: post_password_required( [int|WP_Post|null $post = NULL] )
  * <-......: wp-includes/class-phpass.php: PasswordHash::CheckPassword( string $password, string $stored_hash )
- * @NOW 009: wp-includes/class-phpass.php: PasswordHash::crypt_private( string $password, string $setting )
+ * <-......: wp-includes/class-phpass.php: PasswordHash::crypt_private( string $password, string $setting )
+ * @NOW 010: wp-includes/class-phpass.php: PasswordHash::encode64( string $input, int $count )
  */
+
+	/**
+	 * @param  string $password
+	 * @param  string $setting
+	 * @return string
+	 */
+	function crypt_private( $password, $setting )
+	{
+		$output = '*0';
+
+		if ( substr( $setting, 0, 2 ) == $output ) {
+			$output = '*1';
+		}
+
+		$id = substr( $setting, 0, 3 );
+
+		// We use "$P$", phpBB3 uses "$H$" for the same thing.
+		if ( $id != '$P$' && $id != '$H$' ) {
+			return $output;
+		}
+
+		$count_log2 = strpos( $this->itoa64, $setting[3] );
+
+		if ( $count_log2 < 7 || $count_log2 > 30 ) {
+			return $output;
+		}
+
+		$count = 1 << $count_log2;
+		$salt = substr( $setting, 4, 8 );
+
+		if ( strlen( $salt ) != 8 ) {
+			return $output;
+		}
+
+		/**
+		 * We're kind of forced to use MD5 here since it's the only cryptographic primitive available in all versions of PHP currently in use.
+		 * To implement our own low-level crypto in PHP would result in much worse performance and consequently in lower iteration counts and hashes that are quicker to crack (by non-PHP code).
+		 */
+		if ( PHP_VERSION >= '5' ) {
+			$hash = md5( $salt . $password, TRUE );
+
+			do {
+				$hash = md5( $hash . $password, TRUE );
+			} while ( --$count );
+		} else {
+			$hash = pack( 'H*', md5( $salt . $password ) );
+
+			do {
+				$hash = pack( 'H*', md5( $hash . $password ) );
+			} while ( --$count );
+		}
+
+		$output = substr( $setting, 0, 12 );
+		$output .= $this->encode64( $hash, 16 );
+/**
+ * <-......: wp-blog-header.php
+ * <-......: wp-load.php
+ * <-......: wp-settings.php
+ * <-......: wp-includes/default-filters.php
+ * <-......: wp-includes/formatting.php: wp_trim_excerpt( [string $text = ''] )
+ * <-......: wp-includes/post-template.php: get_the_content( [string $more_link_text = NULL [, bool $strip_teaser = FALSE]] )
+ * <-......: wp-includes/post-template.php: post_password_required( [int|WP_Post|null $post = NULL] )
+ * <-......: wp-includes/class-phpass.php: PasswordHash::CheckPassword( string $password, string $stored_hash )
+ * @NOW 009: wp-includes/class-phpass.php: PasswordHash::crypt_private( string $password, string $setting )
+ * ......->: wp-includes/class-phpass.php: PasswordHash::encode64( string $input, int $count )
+ */
+	}
 
 	/**
 	 * @param  string $password
